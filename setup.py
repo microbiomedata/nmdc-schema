20b234e4f25492d27dc879cd8e53ba3ac1043a19
Bill Duncan <wdduncan@gmail.com>
2021-04-15 18:20:27 -0400

    add validate command line tool to package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="nmdc_schema",
     url="https://github.com/microbiomedata/nmdc-schema",
     # set version manually when testing
-    # version="2021.04.14",
+    # version="2021.04.15a1",
     # use use_scm_version when version is set by github release action
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
@@ -17,6 +17,9 @@ setup(
     author_email="wdduncan@gmail.com",
     license="CC0 1.0 Universal",
     description="Schema resources for the National Microbiome Data Collaborative (NMDC)",
+    entry_points={
+        "console_scripts": ["validate-nmdc-json=nmdc_schema.validate_nmdc_json:cli"]
+    },
     # long_description=long_description,
     # long_description_content_type="text/markdown",
     classifiers=[
