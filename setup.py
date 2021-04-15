f3dbef8743bff48e3627cc7e556953ab9d19c6d0
Bill Duncan <wdduncan@gmail.com>
2021-04-14 21:39:20 -0400

    clean up setup script

--- setup.py
+++ setup.py
@@ -6,20 +6,19 @@ with open("README.md") as f:
 setup(
     name="nmdc_schema",
     url="https://github.com/microbiomedata/nmdc-schema",
+    # set version manually when testing
     # version="2021.04.14",
-    # version is set by github release action
+    # use use_scm_version when version is set by github release action
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    # packages=find_packages(),
     packages=["nmdc_schema"],  # set package manually
-    include_package_data=True,
-    package_data={"nmdc_schema": ["nmdc.schema.json" "gold-to-mixs.sssom.tsv"]},
+    package_data={"nmdc_schema": ["nmdc.schema.json", "gold-to-mixs.sssom.tsv"]},
     author="Bill Duncan",
     author_email="wdduncan@gmail.com",
     license="CC0 1.0 Universal",
     description="Schema resources for the National Microbiome Data Collaborative (NMDC)",
     # long_description=long_description,
-    long_description_content_type="text/markdown",
+    # long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3 :: Only",
