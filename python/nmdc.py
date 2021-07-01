adda62fd322e4051dca0b591b04f353c4c434792
Bill Duncan <wdduncan@gmail.com>
2021-07-01 16:57:00 -0400

    update artifiacts

--- python/nmdc.py
+++ python/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-06-30 12:54
+# Generation date: 2021-07-01 16:56
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
