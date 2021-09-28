8bdc2e5f92302782001bc63b2a8742842d99d7b6
Bill Duncan <wdduncan@gmail.com>
2021-09-28 17:21:53 -0400

    regenerate artifacts

--- python/nmdc.py
+++ python/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-09-28 17:14
+# Generation date: 2021-09-28 17:21
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
