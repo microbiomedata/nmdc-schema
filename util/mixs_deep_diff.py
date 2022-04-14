4083eaee8f10e6651e953a8e328c67586cbc2df2
Mark A. Miller <mamillerpa@gmail.com>
2022-04-14 15:46:58 -0400

    include range types in attrib diff tsv

--- util/mixs_deep_diff.py
+++ util/mixs_deep_diff.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import yaml
 from deepdiff import DeepDiff
+from linkml_runtime import SchemaView
+
+from strsimpy.cosine import Cosine
+
+INCLUDE_DESCRIPTIONS = True
+
+shingle_size = 2
+cosine_obj = Cosine(shingle_size)
 
 old_file = "../src/schema/mixs.yaml"
 new_file = "../src/schema/mixs_6_for_nmdc.yaml"
@@ -30,32 +38,48 @@ def get_slots_diff(deep_diff_obj, delta_type):
         current_path = current_dds.path(output_format="list")
         # print(current_path)
         if (
-                len(current_path) == 3
-                and current_path[0] == "slots"
-                and current_path[2] == "name"
+            len(current_path) == 3
+            and current_path[0] == "slots"
+            and current_path[2] == "name"
         ):
             slots_diff.add(current_path[1])
         if len(current_path) == 2 and current_path[0] == "slots":
             slots_diff.add(current_path[1])
-    slots_diff = list(slots_diff)
-    slots_diff.sort()
-    return slots_diff
+    sd_list = list(slots_diff)
+    sd_list.sort()
+    return sd_list
 
 
-def get_anno_diffs(deep_diff_obj, tsv_file_name):
+def get_anno_diffs(deep_diff_obj, tsv_file_name, incl_descrs=False):
+    old_view = SchemaView(old_file)
+    new_view = SchemaView(new_file)
     lod = []
     vc = deep_diff_obj["values_changed"]
     for current_vc in vc:
         current_path = current_vc.path(output_format="list")
-        if len(current_path) == 3 and current_path[0] == "slots":
-            lod.append(
-                {
-                    "slot": current_path[1],
-                    "attribute": current_path[2],
-                    "old_val": current_vc.t1,
-                    "new_val": current_vc.t2,
-                }
-            )
+        if (
+            len(current_path) == 3
+            and current_path[0] == "slots"
+            and (current_path[2] != "description" or incl_descrs)
+        ):
+            to_append = {
+                "slot": current_path[1],
+                "attribute": current_path[2],
+                "old_val": current_vc.t1,
+                "new_val": current_vc.t2,
+            }
+            if current_path[2] == "description":
+                p0 = cosine_obj.get_profile(current_vc.t1)
+                p1 = cosine_obj.get_profile(current_vc.t2)
+                cos_dist_res = cosine_obj.similarity_profiles(p0, p1)
+                to_append["descr_dist"] = 1 - cos_dist_res
+            if current_path[2] == "range":
+                old_element = old_view.get_element(current_vc.t1)
+                to_append["old_range_type"] = type(old_element)
+                new_element = new_view.get_element(current_vc.t2)
+                to_append["new_range_type"] = type(new_element)
+            lod.append(to_append)
+
     df = pd.DataFrame(lod)
     # pprint.pprint(df)
     df.to_csv(tsv_file_name, sep="\t", index=False)
@@ -73,9 +97,9 @@ added_diff = get_slots_diff(mixs_diff, "added")
 
 removed_diff = get_slots_diff(mixs_diff, "removed")
 
-slots_diff = {"added": added_diff, "removed": removed_diff}
+slots_diff_dict = {"added": added_diff, "removed": removed_diff}
 
 with open(slot_diff_yaml_out, "w") as outfile:
-    yaml.dump(slots_diff, outfile, default_flow_style=False)
+    yaml.dump(slots_diff_dict, outfile, default_flow_style=False)
 
-get_anno_diffs(mixs_diff, anno_tsv_out)
+get_anno_diffs(mixs_diff, anno_tsv_out, INCLUDE_DESCRIPTIONS)
