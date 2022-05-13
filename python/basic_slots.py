b1329f55298c6675c8e19a89488c681df3270413
Mark Andrew Miller <MAM@lbl.gov>
2022-05-13 08:55:22 -0400

    lock in version 4.0.0
    and don't generate python for mixs legacy or new

--- python/basic_slots.py
+++ python/basic_slots.py
@@ -1,5 +1,5 @@
 # Auto generated from basic_slots.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-05-06T11:21:38
+# Generation date: 2022-05-13T08:53:13
 # Schema: NMDC-Basic-Slots
 #
 # id: https://microbiomedata/schema/basic_slots
--- python/core.py
+++ python/core.py
@@ -1,5 +1,5 @@
 # Auto generated from core.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-05-06T11:22:17
+# Generation date: 2022-05-13T08:53:26
 # Schema: NMDC-Core
 #
 # id: https://microbiomedata/schema/core
