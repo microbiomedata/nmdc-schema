73470de063b120d4bbbd2447d41c6db4c993a557
Mark Andrew Miller <MAM@lbl.gov>
2022-08-08 13:50:53 -0400

    biosample part of and sample_link both optional

---
+++ test/test_biosample_instantiation.py
@@ -0,0 +1,67 @@
+import logging
+
+# import traceback
+import unittest
+
+from python.nmdc import (
+    # PersonValue,
+    # CreditAssociation,
+    # Study,
+    Biosample,
+    ControlledTermValue,
+)
+
+
+# from linkml.validators.jsonschemavalidator import JsonSchemaDataValidator
+
+
+# from linkml_runtime.dumpers import yaml_dumper
+
+# todo reimplement with methods?
+
+
+class TestBiosampleInstantiation(unittest.TestCase):
+    def test_with_part_of(self):
+        try:
+            bs = Biosample(
+                id="x",
+                part_of="x",
+                env_broad_scale=ControlledTermValue(),
+                env_local_scale=ControlledTermValue(),
+                env_medium=ControlledTermValue(),
+            )
+            assert type(bs) == Biosample
+        except Exception as e:
+            logging.error(f"Biosample instantiation failed because: {e}")
+
+    def test_add_sample_link(self):
+        try:
+            bs = Biosample(
+                id="x",
+                part_of="x",
+                sample_link="x",
+                env_broad_scale=ControlledTermValue(),
+                env_local_scale=ControlledTermValue(),
+                env_medium=ControlledTermValue(),
+            )
+            assert type(bs) == Biosample
+        except Exception as e:
+            logging.error(f"Biosample instantiation failed because: {e}")
+
+    def test_remove_part_of(self):
+        try:
+            bs = Biosample(
+                id="x",
+                # part_of="x",
+                sample_link="x",
+                env_broad_scale=ControlledTermValue(),
+                env_local_scale=ControlledTermValue(),
+                env_medium=ControlledTermValue(),
+            )
+            assert type(bs) == Biosample
+        except Exception as e:
+            logging.error(f"Biosample instantiation failed because: {e}")
+
+
+if __name__ == "__main__":
+    unittest.main()
