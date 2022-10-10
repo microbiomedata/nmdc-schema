c7b692820ce8ea453fb498b5a3a278da403a6727
Mark A. Miller <MAM@lbl.gov>
2022-10-10 13:06:03 -0400

    no more dump file
    but some changes to depth2 and OntologyTerm id req

--- util/migrate_7_0.py
+++ util/migrate_7_0.py
@@ -1,75 +1,32 @@
 import pprint
+import re
 
 import pendulum as pendulum
+import yaml
 from dotenv import dotenv_values
-import pymongo
+from linkml.validators import JsonSchemaDataValidator
+from linkml_runtime.dumpers import yaml_dumper
+from pymongo import MongoClient
 from linkml_runtime import SchemaView
-from linkml_runtime.dumpers import yaml_dumper, json_dumper
 
-from nmdc_schema.nmdc import Database
+from nmdc_schema.nmdc import Database, Biosample, ControlledTermValue, OntologyClass
 
 dotenv_file = "../local/.env"
 
 schema_file = "../src/schema/nmdc.yaml"
 
-mongodb_suffix = "@localhost:27027/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+remote_mongodb_suffix = \
+    "@localhost:27027/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+repaired_file = "../assets/nmdc_7_0.json"
+
+# assuming no user/pw authentication for now
+dest_mongo_address = "127.0.0.1"
+dest_mongo_port = "27777"
+dest_mongo_dbname = "nmdc_7_0_0"
+excluded_collections = ['@type']
 
 max_collection_bytes = 10000000000
-
-database_obj = Database()
-
-print(f"Loading config from {dotenv_file}")
-
-config = dotenv_values(dotenv_file)
-
-print(f"Loading schema from {schema_file}")
-
-nmdc_view = SchemaView(schema_file)
-
-database_slots = nmdc_view.class_slots("Database")
-
-myclient = pymongo.MongoClient(
-    f"mongodb://{config['MONGO_USER']}:{config['MONGO_PASS']}{mongodb_suffix}"
-)
-
-mydb = myclient["nmdc"]
-
-mongo_collections = mydb.list_collection_names()
-
-collections_to_repair = set(database_slots).intersection(set(mongo_collections))
-
-print("Gathering mongodb collection stats")
-
-collections_to_repair = [
-    'activity_set',
-    'biosample_set',
-    'data_object_set',
-    # 'functional_annotation_set',
-    # 'genome_feature_set',
-    'mags_activity_set',
-    'metabolomics_analysis_activity_set',
-    'metagenome_annotation_activity_set',
-    'metagenome_assembly_set',
-    'metaproteomics_analysis_activity_set',
-    'metatranscriptome_activity_set',
-    'nom_analysis_activity_set',
-    'omics_processing_set',
-    'study_set'
-]
-
-selected_stats = {}
-for collection_name in collections_to_repair:
-    for_selected_stats = {}
-
-    collection_stats = mydb.command("collstats", collection_name)
-
-    for_selected_stats["document_count"] = collection_stats["count"]
-    for_selected_stats["size_in_bytes"] = collection_stats["size"]
-    for_selected_stats["storageSize"] = collection_stats["storageSize"]
-
-    selected_stats[collection_name] = for_selected_stats
-
-sorted_selected_stats = sorted(selected_stats.items(), key=lambda x: x[1]['storageSize'])
 
 # PROBLEM CASE: slot name has been changed since data was saved
 # todo right now only works for root-level slots within each document
@@ -108,6 +65,68 @@ name_replacements = {
 #  - break raw values from env_broad_scale etc into the term name and the term id
 #  - collapse depth2 into depth
 
+database_obj = Database()
+
+print(f"Loading config from {dotenv_file}")
+
+config = dotenv_values(dotenv_file)
+
+print(f"Loading schema from {schema_file}")
+
+nmdc_view = SchemaView(schema_file)
+
+# nmdc_view.merge_imports()
+
+val_inst = JsonSchemaDataValidator(schema_file)
+
+# todo assertion of mixs triad terms are required for Biosample, but empty ControlledTermValue OK
+#   assertion of id for OntologyClass but empty string OK
+
+database_slots = nmdc_view.class_slots("Database")
+
+remote_client = MongoClient(
+    f"mongodb://{config['MONGO_USER']}:{config['MONGO_PASS']}{remote_mongodb_suffix}"
+)
+
+remote_db = remote_client["nmdc"]
+
+remote_collections = remote_db.list_collection_names()
+
+collections_intersection = set(database_slots).intersection(set(remote_collections))
+
+print("Gathering mongodb collection stats")
+
+collections_to_repair = [
+    'activity_set',
+    'biosample_set',
+    'data_object_set',
+    # 'functional_annotation_set',
+    # 'genome_feature_set',
+    'mags_activity_set',
+    'metabolomics_analysis_activity_set',
+    'metagenome_annotation_activity_set',
+    'metagenome_assembly_set',
+    'metaproteomics_analysis_activity_set',
+    'metatranscriptome_activity_set',
+    'nom_analysis_activity_set',
+    'omics_processing_set',
+    'study_set'
+]
+
+selected_stats = {}
+for collection_name in collections_to_repair:
+    for_selected_stats = {}
+
+    collection_stats = remote_db.command("collstats", collection_name)
+
+    for_selected_stats["document_count"] = collection_stats["count"]
+    for_selected_stats["size_in_bytes"] = collection_stats["size"]
+    for_selected_stats["storageSize"] = collection_stats["storageSize"]
+
+    selected_stats[collection_name] = for_selected_stats
+
+sorted_selected_stats = sorted(selected_stats.items(), key=lambda x: x[1]['storageSize'])
+
 print("starting dumps from mongodb")
 
 data_object_file_type_values = set()
@@ -126,7 +145,7 @@ for collection_name in collections_to_repair:
         print(f"Over max_collection_bytes limit of {max_collection_bytes} for migration: {collection_stats}")
     else:
         print(f"Within max_collection_bytes limit of {max_collection_bytes} for migration:  {collection_stats}")
-        collection = mydb[collection_name]
+        collection = remote_db[collection_name]
         cursor = collection.find({})
         for document in cursor:
             del document["_id"]
@@ -137,14 +156,13 @@ for collection_name in collections_to_repair:
                         del document[old_name]
             if collection_name == "data_object_set" and "data_object_type" in document:
                 data_object_file_type_values.add(document["data_object_type"])
-
             else:
                 pass
+
             # PROBLEM CASE: improperly formatted dates
             # todo remove need for enumerating date fields
             if 'started_at_time' in document:
                 sadt = str(pendulum.parse(document['started_at_time']))
-                # print(sadt)
                 document['started_at_time'] = sadt
             if 'ended_at_time' in document:
                 eadt = str(pendulum.parse(document['ended_at_time']))
@@ -160,7 +178,57 @@ for collection_name in collections_to_repair:
                     if 'num_tRNA' in i:
                         i['num_t_rna'] = i['num_tRNA']
                         del i['num_tRNA']
-                    # pprint.pprint(i)
+
+            # PROBLEM CASE: depth2 will be removed from the schema
+            if collection_name == "biosample_set":
+                if "depth2" in document:
+                    # print(f"depth: {document['depth']}")
+                    if "depth" in document:
+                        # print(f"depth2: {document['depth2']}")
+                        if document['depth']['has_unit'] != document['depth2']['has_unit']:
+                            print(
+                                f"PROBLEM: depth and depth2 have different units: {document['depth']['has_unit']} and {document['depth2']['has_unit']}")
+                        else:
+                            # print(f"depth and depth2 have same units: {document['depth']['has_unit']}")
+                            if 'has_numeric_value' in document['depth2']:
+                                if 'has_maximum_numeric_value' in document['depth']:
+                                    if document['depth']['has_maximum_numeric_value'] != document['depth2'][
+                                        'has_numeric_value']:
+                                        print(
+                                            f"PROBLEM: depth and depth2 have different maximum values: {document['depth']['has_maximum_numeric_value']} and {document['depth2']['has_maximum_numeric_value']}")
+                                    else:
+                                        del document['depth2']
+                                else:
+                                    # todo any assumptions?
+                                    document['depth']['has_maximum_numeric_value'] = document['depth2'][
+                                        'has_numeric_value']
+                                    del document['depth2']
+                    else:
+                        print("depth2 but no depth")
+                        # todo assuming the depth2 object has all of these fields
+                        document['depth'] = {'has_numeric_value': document['depth']['has_numeric_value'],
+                                             'has_raw_value': document['depth']['has_raw_value'],
+                                             'has_unit': document['depth']['has_unit']}
+                        del document['depth2']
+                else:
+                    pass
+                    # print("SURPRISE: no depth2")
+                # PROBLEM CASE: MIXS env triad objects must include a OntologyTerm id
+                #  would like to include the name/label, too
+                #  this causes breakage in 'omics_processing_set'[]['omics_type']
+                # https://microbiomedata.github.io/nmdc-schema/ControlledTermValue/
+                for triad_slot in ['env_broad_scale', 'env_local_scale', 'env_medium']:
+                    if 'term' not in document[triad_slot]:
+                        if 'has_raw_value' not in document[triad_slot]:
+                            print(f"  PROBLEM: no has_raw_value in {triad_slot}")
+                        else:
+                            # todo how much flexibility for the ontology prefix?
+                            m = re.search(r'\b(.*:\d+)', document[triad_slot]['has_raw_value'])
+                            if m:
+                                # todo check for multiple matches
+                                document[triad_slot]['term'] = {'id': m.group(0)}
+                            else:
+                                print(f"    no ENVO:(\\d+) in {triad_slot} has_raw_value")
 
             database_obj[collection_name].append(document)
         if len(database_obj[collection_name]) == 0:
@@ -173,10 +241,32 @@ unexpected_object_type_values = list(data_object_file_type_values - defined_obje
 
 unexpected_object_type_values.sort()
 
-# print(unexpected_object_type_values)
+local_client = MongoClient(
+    f"mongodb://{dest_mongo_address}:{dest_mongo_port}"
+)
 
-print("dumping to json")
+local_db = local_client[dest_mongo_dbname]
 
-json_dumper.dump(database_obj, "mongo_dump.json")
+local_collections = local_db.list_collection_names()
+
+print("attempting to validate repaired data")
+val_inst = JsonSchemaDataValidator(schema_file)
+val_inst.validate_object(database_obj)
+print("data validation complete")
+
+for i in collections_to_repair:
+    if i in excluded_collections:
+        print(f"Skipping {i}")
+    else:
+        if i in remote_collections:
+            print(f"Dropping local {i}")
+            collection = local_db[i]
+            collection.drop()
+        if i in database_obj and database_obj[i]:
+            print(f"Inserting {i}")
+            insertion = collection.insert_many(database_obj[i])
+            print(f"Inserted {len(insertion.inserted_ids)} documents")
+        else:
+            print(f"Skipping {i} because it has no documents")
 
 print("done")
