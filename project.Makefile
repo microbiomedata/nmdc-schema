f2c959a35cdd470adc8644d8777a557f676c7f17
Mark A. Miller <MAM@lbl.gov>
2023-01-26 16:55:45 -0500

    exhasutive biosample done

--- project.Makefile
+++ project.Makefile
@@ -9,10 +9,18 @@ examples-all: examples-clean src/data/output
 examples-clean:
 	rm -rf src/data/output
 
-src/data/output:
+project/nmdc_schema_generated.yaml: $(SOURCE_SCHEMA_PATH)
+	# the need for this may be eliminated by adding mandatory pattern materialization to gen-json-schema
+	$(RUN) gen-linkml \
+		--output $@ \
+		--materialize-patterns \
+		--no-materialize-attributes \
+		--format yaml $<
+
+src/data/output: project/nmdc_schema_generated.yaml
 	mkdir -p $@
 	$(RUN) linkml-run-examples \
-		--schema $(SOURCE_SCHEMA_PATH) \
+		--schema $< \
 		--input-directory src/data/valid \
 		--counter-example-input-directory src/data/invalid \
 		--output-directory $@ > $@/README.md
--- tests/test_report_biosample_slots.py
+++ tests/test_report_biosample_slots.py
@@ -1,17 +1,13 @@
 """Data test."""
 import csv
 import os
-# import glob
-import pprint
 import unittest
 
 from linkml_runtime import SchemaView
 
-# from nmdc_schema.datamodel.nmdc import Biosample
-
 ROOT = os.path.join(os.path.dirname(__file__), '..')
-SCHEMA_DIR = os.path.join(ROOT, "src", "nmdc_schema", "schema")
-SCHEMA_FILE = os.path.join(SCHEMA_DIR, 'nmdc.yaml')
+PROJ_DIR = os.path.join(ROOT, "project")
+SCHEMA_FILE = os.path.join(PROJ_DIR, 'nmdc_schema_generated.yaml')
 REPORTS_DIR = os.path.join(ROOT, "reports")
 BIOSAMPLE_SLOTS_RANGES_FILE = os.path.join(REPORTS_DIR, 'biosample_slots_ranges_report.tsv')
 
@@ -33,12 +29,17 @@ class BiosampleSlotReport(unittest.TestCase):
             slot_range = v.range
             slot_multivalued = v.multivalued
             slot_pattern = v.pattern
+            slot_min = v.minimum_value
+            slot_max = v.maximum_value
+            slot_schema = v.from_schema
+            # slot_schema = is constant after using gen-linkml
             # print(f"{slot_name} {slot_range}")
             slot_ranges.append({"slot_name": slot_name, "slot_range": slot_range, "slot_multivalued": slot_multivalued,
-                                "slot_pattern": slot_pattern})
+                                "slot_pattern": slot_pattern, "slot_min": slot_min, "slot_max": slot_max})
+            # , "slot_min": slot_min, "slot_max": slot_max,
+            #                                 "slot_schema": slot_schema
 
-            # pprint.pprint(slot_ranges)
-
+            # todo delete previous BIOSAMPLE_SLOTS_RANGES_FILE ? or explicitly overwrite
             with open(BIOSAMPLE_SLOTS_RANGES_FILE, 'w', newline='') as output_file:
                 dict_writer = csv.DictWriter(output_file, fieldnames=slot_ranges[0].keys(), delimiter='\t')
                 dict_writer.writeheader()
