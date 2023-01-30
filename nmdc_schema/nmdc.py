4d2976af36d3f714d7394939abc694a30659bd08
Mark A. Miller <MAM@lbl.gov>
2023-01-30 12:09:07 -0500

    regen with nmdc_schema at root

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-01-30T12:05:23
+# Generation date: 2023-01-30T12:06:09
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
