66fdb830a1d3f7a196a9e4842e95674cd81a5e1d
Mark A. Miller <MAM@lbl.gov>
2023-01-31 08:53:06 -0500

    copying artifacts to include

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-01-31T08:47:57
+# Generation date: 2023-01-31T08:48:42
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
