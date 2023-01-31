7da144dadbd73b99b2eae82fb7fd939d8a54625b
Mark A. Miller <MAM@lbl.gov>
2023-01-31 09:25:01 -0500

    Initial commit

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-01-31T09:13:04
+# Generation date: 2023-01-31T09:24:37
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
