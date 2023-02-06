b79986a8f3386db47a5a4a34aa036cfb8eafb290
Mark A. Miller <MAM@lbl.gov>
2023-02-06 15:22:08 -0500

    out of control regen

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-02-06T15:07:53
+# Generation date: 2023-02-06T15:20:09
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
