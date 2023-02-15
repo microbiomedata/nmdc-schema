76530ecdba690aebda7925ce3a3ff6f4f879f4e1
Mark A. Miller <mamillerpa@gmail.com>
2023-02-15 14:26:04 -0500

    regen for v7.4.10 WITH A V

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-02-15T14:10:13
+# Generation date: 2023-02-15T14:22:38
 # Schema: NMDC
 #
 # id: https://microbiomedata/schema
@@ -28,7 +28,7 @@ from linkml_runtime.linkml_model.types import Boolean, Datetime, Double, Float, 
 from linkml_runtime.utils.metamodelcore import Bool, URIorCURIE, XSDDateTime
 
 metamodel_version = "1.7.0"
-version = "7.4.10"
+version = "v7.4.10"
 
 # Overwrite dataclasses _init_fn to add **kwargs in __init__
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
