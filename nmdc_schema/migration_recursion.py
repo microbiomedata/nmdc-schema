d4bf8656f32cb80243f3bb582faad95162e37dc0
eecavanna <eecavanna@users.noreply.github.com>
2023-10-19 18:56:32 -0700

    Implement an example Migrator for tutorial purposes

--- nmdc_schema/migration_recursion.py
+++ nmdc_schema/migration_recursion.py
@@ -187,6 +187,77 @@ class Migrator_from_7_8_0_to_8_0_0(MigratorBase):
             study[field_name] = []
         return study
 
+class Migrator_from_A_B_C_to_X_Y_Z(MigratorBase):
+    """
+    TUTORIAL: This is an example of a "migrator" class.
+              It was designed for use during developer training and
+              to serve as a template for production "migrator" classes.
+    """
+    
+    def __init__(self) -> None:
+        """
+        TUTORIAL: This is the "constructor" function of the class.
+                  As is true about the "constructor" function of any class,
+                  it runs whenever that class is instantiated, and its job
+                  is to initialize the instance of that class.
+                  
+                  When this function runs, it does two things:
+                  1. It invokes the base class's "constructor" function; and
+                  2. It populates a dictionary that keeps track of
+                     which transformation functions will be run on
+                     dictionaries from which collections. You can
+                     think of this dictionary as a "registry."
+
+             -->  As part of creating a new "migration" class, you will
+                  typically customize the contents of that dictionary.
+        """
+
+        # Invoke the base class's constructor function.
+        super().__init__()
+
+        # Map collections to this class's transformation functions.
+        self.transformers_by_collection = dict(
+            study_set=[self.allow_multiple_names],
+        )
+
+    def allow_multiple_names(self, study: dict) -> dict:
+        """
+        TUTORIAL: This is an example "transformation" function within the class.
+                  Its jobs is to transform a dictionary that conforms to the
+                  initial schema version (in this case, version "A.B.C"), into one
+                  that conforms to the target schema version (in this case,
+                  version "X.Y.Z"). That might involve adding a field,
+                  converting a string into a list of strings, etc.
+                  
+                  When this function runs, it does three things:
+                  1. It accepts a single dictionary as a parameter.
+                  2. It transforms that dictionary.
+                  3. It returns the transformed dictionary.
+
+              --> As part of creating a new "migration" class, you will
+                  typically implement one or more "transformation" functions.
+                  You will also add them to the "registry" of the class.
+        """
+
+        logger.info(f"Transforming study having id: {study['id']}")  # optional log message
+
+        # Transform the dictionary.
+        #
+        # TUTORIAL: In this example scenario, I am pretending that:
+        #           1. In schema version "A.B.C", the `Study` class has a `name` slot,
+        #              which contain a single string.
+        #           2. In schema version "X.Y.Z", the `Study` class no longer has that
+        #              `name` slot. Instead, it has a `names` slot, which contains
+        #              a list of strings.
+        #
+        original_name = study["name"]  # preserve the original value
+        study["names"] = []  # create a new key, whose value is an empty list of names
+        study["names"].append(original_name)  # add the original value to that list
+        del study["name"]  # delete the obsolete key
+
+        # Return the transformed dictionary.
+        return study
+
 
 @click.command()
 @click_log.simple_verbosity_option(logger)
