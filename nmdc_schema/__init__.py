c144c5461327d1a6596d604d3aabb940cc1c4e5c
eecavanna <eecavanna@users.noreply.github.com>
2023-11-11 20:27:00 -0800

    WIP: Bind migration-specific modules to `nmdc_schema` package namespace explicitly

--- nmdc_schema/__init__.py
+++ nmdc_schema/__init__.py
@@ -4,4 +4,9 @@
 # - https://py-pkgs.org/04-package-structure.html#the-init-file (about the `__init__.py` file)
 # - https://github.com/python-poetry/poetry/issues/2270 (an issue regarding Poetry's support for sub-packages)
 #
-from nmdc_schema import migrators
+from nmdc_schema.migrators import (
+    migrator_from_7_7_2_to_7_8_0,
+    migrator_from_7_8_0_to_8_0_0,
+    migrator_from_8_0_to_8_1,
+    migrator_from_8_1_to_9_0
+)
