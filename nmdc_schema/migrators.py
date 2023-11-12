115e1d75804a4eae45e126ac00f61fbb509bdb20
eecavanna <eecavanna@users.noreply.github.com>
2023-11-11 20:55:13 -0800

    Revert all "WIP" changes related to exposing sub-package's contents

--- nmdc_schema/migrators.py
+++
@@ -1,7 +0,0 @@
-# Import modules (residing in a sub-package) that we want to be accessible from outside the `nmdc_schema` package.
-from nmdc_schema.migrators import (
-    migrator_from_7_7_2_to_7_8_0,
-    migrator_from_7_8_0_to_8_0_0,
-    migrator_from_8_0_to_8_1,
-    migrator_from_8_1_to_9_0,
-)
