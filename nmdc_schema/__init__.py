60548841964a65cd5c165c6287b03d29f6fdb601
eecavanna <eecavanna@users.noreply.github.com>
2023-11-11 20:12:58 -0800

    WIP: Expose `migrators` package to external code

--- nmdc_schema/__init__.py
+++ nmdc_schema/__init__.py
@@ -4,4 +4,4 @@
 # - https://py-pkgs.org/04-package-structure.html#the-init-file (about the `__init__.py` file)
 # - https://github.com/python-poetry/poetry/issues/2270 (an issue regarding Poetry's support for sub-packages)
 #
-import nmdc_schema.migrators
+from nmdc_schema import migrators
