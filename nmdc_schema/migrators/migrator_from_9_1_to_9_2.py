3fca752117c39f18d1117bf9704724a0be4cf700
brynn.zalmanek@pnnl.gov <brynn.zalmanek@pnnl.gov>
2023-11-22 11:16:57 -0800

    fix indentation issues

--- nmdc_schema/migrators/migrator_from_9_1_to_9_2.py
+++ nmdc_schema/migrators/migrator_from_9_1_to_9_2.py
@@ -16,14 +16,17 @@ class Migrator_from_9_1_to_9_2(MigratorBase):
 
     def move_doi_from_websites(self, study: dict):
         r"""
-        Transforms `websites` values that are dois into curies and moves them into the `associated_dois slot
-        Removes the doi website values from the `websites slot.
-        
+        Transforms `websites` values that are dois into curies and moves them into the `associated_dois` slot
+        Removes the doi website values from the `websites` slot.
+
         >>> m = Migrator_from_9_1_to_9_2()
-        >>> m.move_doi_from_websites({'id': 123, 'websites': ['a', 'b/doi.org/10.23'], 'associated_dois': 
-        ...     [{'doi_value': 'j', 'doi_provider': 'k', 'doi_category': 'i'}]})
-        {'id': 123, 'websites': ['a'], 'associated_dois': [{'doi_value': 'j', 'doi_provider': 'k', 
-        ...     'doi_category': 'i'}, {'doi_value': 'doi:10.23', 'doi_category': 'x', 'doi_provider': 'y'}]}"""
+        >>> m.move_doi_from_websites({'id': 123, 'websites': ['a', 'b/doi.org/10.23'], 'associated_dois':
+        ...                            [{'doi_value': 'j', 'doi_provider': 'k', 'doi_category': 'i'}]})
+        {'id': 123, 'websites': ['a'], 'associated_dois': [{'doi_value': 'j', 'doi_provider': 'k',
+                                                            'doi_category': 'i'},
+                                                        {'doi_value': 'doi:10.23', 'doi_category': 'x',
+                                                            'doi_provider': 'y'}]}
+        """
 
         doi_updates = load_yaml_asset('migrator_from_9_1_to_9_2/websites_dois.yaml')
 
