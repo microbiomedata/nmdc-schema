8f1443dd689eb7b54d879146bf592dd97bf5b182
Mark A. Miller <MAM@lbl.gov>
2023-12-11 14:03:31 -0500

    really revert "fluid not solution"
    This reverts commit a0b9fab3f0f5a17931e6621b8907142accdf60d8.

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -5,7 +5,7 @@ from linkml.validators.jsonschemavalidator import JsonSchemaDataValidator
 from linkml_runtime.dumpers import yaml_dumper
 from linkml_runtime.loaders import yaml_loader
 
-from nmdc_schema.nmdc import ConstituentOfFluid, QuantityValue, Fluid, Extraction, ChromatographicSeparationProcess
+from nmdc_schema.nmdc import SolutionComponent, QuantityValue, Solution, Extraction, ChromatographicSeparationProcess
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 SCHEMA_DIR = os.path.join(ROOT, "src", "schema")
@@ -17,37 +17,37 @@ class TestExtraction(unittest.TestCase):
     def test_extraction(self):
         # validator = JsonSchemaDataValidator(SCHEMA_FILE)  # it takes a long time to construct this
 
-        component1 = ConstituentOfFluid(
-            name="deionized water",
+        component1 = SolutionComponent(
+            compound="deionized water",
         )
-        # validator.validate_object(component1, target_class=ConstituentOfFluid)
+        # validator.validate_object(component1, target_class=SolutionComponent)
 
         qv2 = QuantityValue(has_numeric_value=10, has_unit="%")
         # validator.validate_object(qv2, target_class=QuantityValue)
 
-        component2 = ConstituentOfFluid(
-            name="methanol",
+        component2 = SolutionComponent(
+            compound="deionized water",
             concentration=qv2
         )
-        # validator.validate_object(component2, target_class=ConstituentOfFluid)
+        # validator.validate_object(component2, target_class=SolutionComponent)
 
-        fluid_1 = Fluid(
-            has_constituents=[component1, component2],
+        sol1 = Solution(
+            has_solution_components=[component1, component2],
             volume=QuantityValue(
                 has_numeric_value=100,
                 has_unit="mL"
             )
         )
-        # validator.validate_object(fluid_1, target_class=Fluid)
+        # validator.validate_object(sol1, target_class=Solution)
 
         ex1 = Extraction(
             id="nmdc:ILLEGAL_ID_VAL",
             has_input=["nmdc:ILLEGAL_ID_VAL"],
             has_output=["nmdc:ILLEGAL_ID_VAL"],
-            extractant=fluid_1,
+            extractant=sol1,
         )
 
-        # validator.validate_object(fluid_1, target_class=Fluid)
+        # validator.validate_object(sol1, target_class=Solution)
 
         print("\n")
         print(ex1)
@@ -62,9 +62,9 @@ has_input:
 has_output:
 - nmdc:ILLEGAL_ID_VAL
 extractant:
-  has_constituents:
-  - name: deionized water
-  - name: methanol
+  has_solution_components:
+  - compound: deionized water
+  - compound: deionized water
     concentration:
       has_numeric_value: 10.0
       has_unit: '%'
@@ -81,22 +81,22 @@ class TestChromatographySeparationProcess(unittest.TestCase):
     def test_phase_separation(self):
         # validator = JsonSchemaDataValidator(SCHEMA_FILE)  # it takes a long time to construct this
 
-        component1 = ConstituentOfFluid(
-            name="deionized water",
+        component1 = SolutionComponent(
+            compound="deionized water",
         )
-        # validator.validate_object(component1, target_class=ConstituentOfFluid)
+        # validator.validate_object(component1, target_class=SolutionComponent)
 
         qv2 = QuantityValue(has_numeric_value=10, has_unit="%")
         # validator.validate_object(qv2, target_class=QuantityValue)
 
-        component2 = ConstituentOfFluid(
-            name="methanol",
+        component2 = SolutionComponent(
+            compound="methanol",
             concentration=qv2
         )
-        # validator.validate_object(component2, target_class=ConstituentOfFluid)
+        # validator.validate_object(component2, target_class=SolutionComponent)
 
-        fluid_1 = Fluid(
-            has_constituents=[component1, component2],
+        sol1 = Solution(
+            has_solution_components=[component1, component2],
             volume=QuantityValue(
                 has_numeric_value=100,
                 has_unit="mL"
@@ -105,7 +105,7 @@ class TestChromatographySeparationProcess(unittest.TestCase):
 
         ps1 = ChromatographicSeparationProcess(
             id="nmdc:ILLEGAL_ID_VAL",
-            ordered_mobile_phases=fluid_1,
+            ordered_mobile_phases=sol1,
             stationary_phase="C8",
         )
 
