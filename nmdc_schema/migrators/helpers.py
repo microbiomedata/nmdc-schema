ee67e8a2f90fe0e6f181f11d43678d545eea14b5
eecavanna <eecavanna@users.noreply.github.com>
2023-12-13 15:09:31 -0800

    Delete (unused) functions that find migrator module for schema version

--- nmdc_schema/migrators/helpers.py
+++ nmdc_schema/migrators/helpers.py
@@ -1,57 +1,14 @@
 import logging
-from typing import Any, Type, Optional, List
+from typing import Any
 from importlib import resources
 from pathlib import Path
-from migrator_base import MigratorBase
 import yaml
-import pkgutil
-import importlib
 
 logger = logging.getLogger(__name__)
 
 # TODO: Determine directory path and package path dynamically, instead of hard-coding them.
 MIGRATOR_DIRECTORY = "nmdc_schema/migrators"
 MIGRATOR_PACKAGE = "nmdc_schema.migrators"
-
-
-def get_migrator_module_names() -> List[str]:
-    """
-    Returns a list of all migrator module names.
-    """
-
-    module_names = [name for _, name, _ in pkgutil.iter_modules([MIGRATOR_DIRECTORY])]
-    migrator_module_names = [s for s in module_names if s.startswith("migrator_from_")]  # filters the list
-    return migrator_module_names
-
-
-def get_migrator_to(destination_version: str) -> Optional[Type[MigratorBase]]:
-    """
-    Returns a migrator class, if any, that migrates data to the specified schema version.
-    """
-
-    matching_migrator_class = None
-    for module_name in get_migrator_module_names():
-        try:
-            # Import the module having that name.
-            # Reference: https://docs.python.org/3/library/importlib.html#importlib.import_module
-            migrator_module = importlib.import_module(f".{module_name}", package=MIGRATOR_PACKAGE)
-
-            # Get the `Migrator` class defined in that module.
-            # Reference: https://docs.python.org/3/library/inspect.html#inspect.isclass
-            migrator_class = getattr(migrator_module, "Migrator")
-            if not issubclass(migrator_class, MigratorBase):
-                raise TypeError
-
-            # If that migrator class migrates data to the specified version, return the class.
-            if migrator_class.get_destination_version() == destination_version:
-                matching_migrator_class = migrator_class
-                break  # no need to check other classes
-
-        except (KeyError, ImportError, AttributeError, TypeError) as exception:
-            logger.exception(f"Failed to get migrator. {exception}.")
-
-    logger.warning(f"No compatible migrator found.")
-    return matching_migrator_class
 
 
 def load_yaml_asset(path_to_asset_file: str) -> Any:
