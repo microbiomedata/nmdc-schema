bc141fe64b62d5b138ee912fc74ddd428009d38b
brynnz22 <brynn.zalmanek@pnnl.gov>
2024-01-05 09:59:24 -0800

    update function names in transformation pipeline calls

--- nmdc_schema/migrators/migrator_from_X_to_PR23.py
+++ nmdc_schema/migrators/migrator_from_X_to_PR23.py
@@ -11,16 +11,16 @@ class Migrator_from_X_to_PR23(MigratorBase):
 
         # Populate the "collection-to-transformers" map for this specific migration.
         self.agenda = dict(
-            metagenome_assembly_set=[self.execution_resource_update], 
-            metagenome_annotation_activity_set=[self.execution_resource_update],
-            metatranscriptome_activity_set=[self.execution_resource_update], 
-            mags_activity_set=[self.execution_resource_update],
-            metagenome_sequencing_activity_set=[self.execution_resource_update], 
-            read_qc_analysis_activity_set=[self.execution_resource_update],
-            read_based_taxonomy_analysis_activity_set=[self.execution_resource_update], 
-            metabolomics_analysis_activity_set=[self.execution_resource_update],
-            metaproteomics_analysis_activity_set=[self.execution_resource_update], 
-            nom_analysis_activity_set=[self.execution_resource_update]
+            metagenome_assembly_set=[self.standardize_execution_resource], 
+            metagenome_annotation_activity_set=[self.standardize_execution_resource],
+            metatranscriptome_activity_set=[self.standardize_execution_resource], 
+            mags_activity_set=[self.standardize_execution_resource],
+            metagenome_sequencing_activity_set=[self.standardize_execution_resource], 
+            read_qc_analysis_activity_set=[self.standardize_execution_resource],
+            read_based_taxonomy_analysis_activity_set=[self.standardize_execution_resource], 
+            metabolomics_analysis_activity_set=[self.standardize_execution_resource],
+            metaproteomics_analysis_activity_set=[self.standardize_execution_resource], 
+            nom_analysis_activity_set=[self.standardize_execution_resource]
         )
 
     def standardize_execution_resource(self, workflow_execution: dict):
@@ -51,7 +51,7 @@ class Migrator_from_X_to_PR23(MigratorBase):
             
             if matches:
 
-                if difflib.SequenceMatcher(None, workflow_resource_value, matches[0]).ratio() < 1:
+                if workflow_resource_value != matches[0]:
                     # change value of the workflow_execution resource to the proper permissible value if the match is not 100%
                     self.logger.info(f"Changing the workflow {workflow_id} execution_resource value from {workflow_resource_value} to {matches[0]}")
                     workflow_execution['execution_resource'] = matches[0]
