09e912d1fcc21d9915a848ed1c68082d87f9d1bb
eecavanna <eecavanna@users.noreply.github.com>
2024-01-09 18:35:58 -0800

    Explicitly indicate the base adapter is an Abstract Base Class

--- nmdc_schema/migrators/adapters/adapter_base.py
+++ nmdc_schema/migrators/adapters/adapter_base.py
@@ -1,30 +1,34 @@
+from abc import ABC, abstractmethod
 from typing import Optional
 
 
-class AdapterBase:
+class AdapterBase(ABC):
     r"""
-    Base class containing method stubs related to manipulating a "database" represented by either
+    Abstract base class containing method stubs related to manipulating a "database" represented by either
     a Python data dictionary or a MongoDB database.
     """
 
+    @abstractmethod
     def rename_collection(
         self, current_collection_name: str, new_collection_name: str
     ) -> None:
         r"""
         Renames the specified collection so that it has the specified name.
         """
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def get_document_by_nmdc_id(
         self, collection_name: str, nmdc_id: str
     ) -> Optional[dict]:
         r"""
         Retrieves the document having the specified `id` value, from the collection having the specified name.
         """
-        raise NotImplementedError()
+        pass
 
+    @abstractmethod
     def insert_document(self, collection_name: str, document: dict) -> None:
         r"""
         Inserts the specified document into the collection having the specified name.
         """
-        raise NotImplementedError()
+        pass
