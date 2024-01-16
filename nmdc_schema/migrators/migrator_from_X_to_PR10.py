b451844e4777d65255fe5b2b8f5b054c965c54fa
brynnz22 <brynn.zalmanek@pnnl.gov>
2024-01-16 15:49:18 -0800

    add schemaview changes (instead of hardcoded dict)

--- nmdc_schema/migrators/migrator_from_X_to_PR10.py
+++ nmdc_schema/migrators/migrator_from_X_to_PR10.py
@@ -1,4 +1,5 @@
 from nmdc_schema.migrators.migrator_base import MigratorBase
+from linkml_runtime import SchemaView
 
 class Migrator_from_X_to_PR10(MigratorBase):
     """Migrates data from schema X to PR10"""
@@ -73,93 +74,110 @@ class Migrator_from_X_to_PR10(MigratorBase):
         # Adds the type slot with the correct class_uri as a value to each collection instance
         document["type"] = class_uri
 
-        inlined_slots = {
-            "collection_date": "nmdc:TimestampValue", 
-            "depth": "nmdc:QuantityValue",
-            "env_broad_scale": "nmdc:ControlledIdentifiedTermValue",
-            "env_medium": "nmdc:ControlledIdentifiedTermValue",
-            "geo_loc_name": "nmdc:TextValue",
-            "lat_lon": "nmdc:GeolocationValue",
-            "env_local_scale": "nmdc:ControlledIdentifiedTermValue",
-            "temp": "nmdc:QuantityValue",
-            "nitrate": "nmdc:QuantityValue",
-            "salinity": "nmdc:QuantityValue",
-            "subsurface_depth": "nmdc:QuantityValue",
-            "env_package": "nmdc:TextValue",
-            "host_taxid": "nmdc:ControlledIdentifiedTermValue",
-            "samp_taxon_id": "nmdc:ControlledIdentifiedTermValue",
-            "ammonium_nitrogen": "nmdc:QuantityValue",
-            "calcium": "nmdc:QuantityValue",
-            "lbc_thirty": "nmdc:QuantityValue",
-            "lbceq": "nmdc:QuantityValue",
-            "magnesium": "nmdc:QuantityValue",
-            "manganese": "nmdc:QuantityValue",
-            "nitrate_nitrogen": "nmdc:QuantityValue",
-            "nitrite_nitrogen": "nmdc:QuantityValue",
-            "potassium": "nmdc:QuantityValue",
-            "tot_nitro": "nmdc:QuantityValue",
-            "zinc": "nmdc:QuantityValue",
-            "samp_store_temp": "nmdc:QuantityValue",
-            "sieving": "nmdc:TextValue",
-            "store_cond": "nmdc:TextValue",
-            "carb_nitro_ratio": "nmdc:QuantityValue",
-            "cur_vegetation": "nmdc:TextValue",
-            "tot_phosp": "nmdc:QuantityValue",
-            "growth_facil": "nmdc:ControlledTermValue",
-            "org_carb": "nmdc:QuantityValue",
-            "tot_nitro_content": "nmdc:QuantityValue",
-            "nitro": "nmdc:QuantityValue",
-            "tot_org_carb": "nmdc:QuantityValue",
-            "chem_administration": "nmdc:ControlledTermValue",
-            "gravidity": "nmdc:TextValue",
-            "host_age": "nmdc:QuantityValue",
-            "host_body_habitat": "nmdc:TextValue",
-            "host_body_product": "nmdc:ControlledTermValue",
-            "host_body_site": "nmdc:ControlledTermValue",
-            "host_common_name": "nmdc:TextValue",
-            "host_diet": "nmdc:TextValue",
-            "host_genotype": "nmdc:TextValue",
-            "host_life_stage": "nmdc:TextValue",
-            "perturbation": "nmdc:TextValue",
-            "source_mat_id": "nmdc:TextValue",
-            "experimental_factor": "nmdc:ControlledTermValue",
-            "samp_size": "nmdc:QuantityValue",
-            "abs_air_humidity": "nmdc:QuantityValue",
-            "avg_temp": "nmdc:QuantityValue",
-            "fertilizer_regm": "nmdc:TextValue",
-            "host_dry_mass": "nmdc:QuantityValue",
-            "host_height": "nmdc:QuantityValue",
-            "humidity": "nmdc:QuantityValue",
-            "photon_flux": "nmdc:QuantityValue",
-            "solar_irradiance": "nmdc:QuantityValue",
-            "tot_carb": "nmdc:QuantityValue",
-            "wind_direction": "nmdc:TextValue",
-            "wind_speed": "nmdc:QuantityValue",
-            "ammonium": "nmdc:QuantityValue",
-            "diss_oxygen": "nmdc:QuantityValue", 
-            "chlorophyll": "nmdc:QuantityValue",
-            "diss_inorg_nitro": "nmdc:QuantityValue",
-            "soluble_react_phosp": "nmdc:QuantityValue",
-            "sodium": "nmdc:QuantityValue",
-            "sulfate": "nmdc:QuantityValue",
-            "diss_inorg_carb": "nmdc:QuantityValue",
-            "diss_org_carb": "nmdc:QuantityValue",
-            "chloride": "nmdc:QuantityValue",
-            "diss_iron": "nmdc:QuantityValue",
-            "associated_dois": "nmdc:Doi",
-            "has_credit_associations": "prov:Association",
-            "principal_investigator": "nmdc:PersonValue",
-            "study_image": "nmdc:ImageValue",
-            "input_mass": "nmdc:QuantityValue",
-            "quality_control_report": "nmdc:QualityControlReport"
-            }
-        
-        types_with_inlined_classes = ["nmdc:Biosample", "nmdc:Study", "nmdc:Extraction"]
+        # inlined_slots = {
+        #     "collection_date": "nmdc:TimestampValue", 
+        #     "depth": "nmdc:QuantityValue",
+        #     "env_broad_scale": "nmdc:ControlledIdentifiedTermValue",
+        #     "env_medium": "nmdc:ControlledIdentifiedTermValue",
+        #     "geo_loc_name": "nmdc:TextValue",
+        #     "lat_lon": "nmdc:GeolocationValue",
+        #     "env_local_scale": "nmdc:ControlledIdentifiedTermValue",
+        #     "temp": "nmdc:QuantityValue",
+        #     "nitrate": "nmdc:QuantityValue",
+        #     "salinity": "nmdc:QuantityValue",
+        #     "subsurface_depth": "nmdc:QuantityValue",
+        #     "env_package": "nmdc:TextValue",
+        #     "host_taxid": "nmdc:ControlledIdentifiedTermValue",
+        #     "samp_taxon_id": "nmdc:ControlledIdentifiedTermValue",
+        #     "ammonium_nitrogen": "nmdc:QuantityValue",
+        #     "calcium": "nmdc:QuantityValue",
+        #     "lbc_thirty": "nmdc:QuantityValue",
+        #     "lbceq": "nmdc:QuantityValue",
+        #     "magnesium": "nmdc:QuantityValue",
+        #     "manganese": "nmdc:QuantityValue",
+        #     "nitrate_nitrogen": "nmdc:QuantityValue",
+        #     "nitrite_nitrogen": "nmdc:QuantityValue",
+        #     "potassium": "nmdc:QuantityValue",
+        #     "tot_nitro": "nmdc:QuantityValue",
+        #     "zinc": "nmdc:QuantityValue",
+        #     "samp_store_temp": "nmdc:QuantityValue",
+        #     "sieving": "nmdc:TextValue",
+        #     "store_cond": "nmdc:TextValue",
+        #     "carb_nitro_ratio": "nmdc:QuantityValue",
+        #     "cur_vegetation": "nmdc:TextValue",
+        #     "tot_phosp": "nmdc:QuantityValue",
+        #     "growth_facil": "nmdc:ControlledTermValue",
+        #     "org_carb": "nmdc:QuantityValue",
+        #     "tot_nitro_content": "nmdc:QuantityValue",
+        #     "nitro": "nmdc:QuantityValue",
+        #     "tot_org_carb": "nmdc:QuantityValue",
+        #     "chem_administration": "nmdc:ControlledTermValue",
+        #     "gravidity": "nmdc:TextValue",
+        #     "host_age": "nmdc:QuantityValue",
+        #     "host_body_habitat": "nmdc:TextValue",
+        #     "host_body_product": "nmdc:ControlledTermValue",
+        #     "host_body_site": "nmdc:ControlledTermValue",
+        #     "host_common_name": "nmdc:TextValue",
+        #     "host_diet": "nmdc:TextValue",
+        #     "host_genotype": "nmdc:TextValue",
+        #     "host_life_stage": "nmdc:TextValue",
+        #     "perturbation": "nmdc:TextValue",
+        #     "source_mat_id": "nmdc:TextValue",
+        #     "experimental_factor": "nmdc:ControlledTermValue",
+        #     "samp_size": "nmdc:QuantityValue",
+        #     "abs_air_humidity": "nmdc:QuantityValue",
+        #     "avg_temp": "nmdc:QuantityValue",
+        #     "fertilizer_regm": "nmdc:TextValue",
+        #     "host_dry_mass": "nmdc:QuantityValue",
+        #     "host_height": "nmdc:QuantityValue",
+        #     "humidity": "nmdc:QuantityValue",
+        #     "photon_flux": "nmdc:QuantityValue",
+        #     "solar_irradiance": "nmdc:QuantityValue",
+        #     "tot_carb": "nmdc:QuantityValue",
+        #     "wind_direction": "nmdc:TextValue",
+        #     "wind_speed": "nmdc:QuantityValue",
+        #     "ammonium": "nmdc:QuantityValue",
+        #     "diss_oxygen": "nmdc:QuantityValue", 
+        #     "chlorophyll": "nmdc:QuantityValue",
+        #     "diss_inorg_nitro": "nmdc:QuantityValue",
+        #     "soluble_react_phosp": "nmdc:QuantityValue",
+        #     "sodium": "nmdc:QuantityValue",
+        #     "sulfate": "nmdc:QuantityValue",
+        #     "diss_inorg_carb": "nmdc:QuantityValue",
+        #     "diss_org_carb": "nmdc:QuantityValue",
+        #     "chloride": "nmdc:QuantityValue",
+        #     "diss_iron": "nmdc:QuantityValue",
+        #     "associated_dois": "nmdc:Doi",
+        #     "has_credit_associations": "prov:Association",
+        #     "principal_investigator": "nmdc:PersonValue",
+        #     "study_image": "nmdc:ImageValue",
+        #     "input_mass": "nmdc:QuantityValue",
+        #     "quality_control_report": "nmdc:QualityControlReport"
+        #     }
+
+        view = SchemaView("src/schema/nmdc.yaml")
+
+        # Get list of all classes that are used as ranges for slots (that are applicable to this migration)
+        value_classes = view.class_children("AttributeValue")
+        value_classes.extend(["ControlledIdentifiedTermValue", "Doi", "QualityControlReport"])
+
+        # create a dictionary of inlined class uris
+        value_class_uris = {}
+        for value_class in value_classes:
+            uri = view.get_class(value_class).class_uri
+            value_class_uris[value_class] = uri
+
+        # create dictionary of slots and their range's associated class_uris
+        slots_with_inlined_classes = {}
+        for slot_name, slot_def in view.all_slots().items():
+            if slot_def.range in value_class_uris:
+                slots_with_inlined_classes[slot_name] = value_class_uris[slot_def.range]
+                
+        # types_with_inlined_classes = ["nmdc:Biosample", "nmdc:Study", "nmdc:Extraction"]
         # Add the type slot to any inlined classes in the biosample_set
-        for slot, uri in inlined_slots.items():
-            if class_uri in types_with_inlined_classes:
-                self.add_type_to_inlined_classes(document, slot, uri)
-        
+        for slot, uri in slots_with_inlined_classes.items():
+            self.add_type_to_inlined_classes(document, slot, uri)
+                
         return document
 
     
