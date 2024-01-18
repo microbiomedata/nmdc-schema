9b2763eda2a8c1da073aec158d6822befba90990
eecavanna <eecavanna@users.noreply.github.com>
2024-01-17 17:30:09 -0800

    Delete `quality_control_report` field under all conditions

--- nmdc_schema/migrators/migrator_from_9_3_to_9_4.py
+++ nmdc_schema/migrators/migrator_from_9_3_to_9_4.py
@@ -16,16 +16,23 @@ class Migrator(MigratorBase):
 
     def move_extraction_qc_status(self, extraction: dict) -> dict:
         r"""
-        Move quality_control_report.status to qc_status
+        Updates the `ExtractionSet` so that the value originally in its `quality_control_report.status` field, if any,
+        is stored in a new field named `qc_status`; and the `quality_control_report` field no longer exists.
 
         >>> m = Migrator()
         >>> m.move_extraction_qc_status({'id': 123, 'quality_control_report': {'status': 'pass'}})
         {'id': 123, 'qc_status': 'pass'}
+        >>> m.move_extraction_qc_status({'id': 123, 'quality_control_report': {'status': 'pass', 'name': 'n'}})
+        {'id': 123, 'qc_status': 'pass'}
+        >>> m.move_extraction_qc_status({'id': 123, 'quality_control_report': {'name': 'n'}})
+        {'id': 123}
+        >>> m.move_extraction_qc_status({'id': 123, 'quality_control_report': {}})
+        {'id': 123}
         """
         self.logger.info(f"Starting migration of {extraction['id']}")
         if 'quality_control_report' in extraction:
             if 'status' in extraction['quality_control_report']:
                 extraction['qc_status'] = extraction['quality_control_report']['status']
                 del extraction['quality_control_report']['status']
-                del extraction['quality_control_report']
+            del extraction['quality_control_report']
         return extraction
