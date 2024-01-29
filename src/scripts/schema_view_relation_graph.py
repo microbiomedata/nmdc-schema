a6140d4e44944c2ed9d20e6edb6df5182aea3668
Mark A. Miller <MAM@lbl.gov>
2024-01-29 14:33:26 -0500

    include attribute value ranges

--- src/scripts/schema_view_relation_graph.py
+++ src/scripts/schema_view_relation_graph.py
@@ -17,46 +17,6 @@ from linkml_runtime.dumpers import yaml_dumper
 #   actually it looks like just about everything should be expressed with a label-based URI
 #     in order to integrate with the generated owl
 
-### materialization
-# OBI:0000011 dcterms:description xsd:string
-# nmdc:Database nmdc:planned_process_set OBI:0000011
-# etc
-
-### OWL
-# nmdc:PlannedProcess a owl:Class,
-#     skos:exactMatch OBI:0000011 ;
-
-### materialization
-# prov:Association nmdc:applied_roles nmdc:credit%20enum ;
-
-### OWL
-# nmdc:CreditEnum a owl:Class,
-
-# nmdc:Biosample dcterms:description xsd:string ;
-#     dcterms:isPartOf nmdc:NamedThing ;
-#     MIXS:0000001 nmdc:QuantityValue ;
-
-# nmdc:DataObject dcterms:description xsd:string ;
-#     nmdc:alternative_identifiers xsd:anyURI ;
-#     nmdc:compression_type xsd:string ;
-#     nmdc:data_object_type nmdc:file%20type%20enum ;
-
-# nmdc:OmicsProcessing dcterms:description xsd:string ;
-#     dcterms:isPartOf nmdc:NamedThing ;
-#     MIXS:0000037 nmdc:TextValue ;
-
-# nmdc:FieldResearchSite dcterms:description xsd:string ;
-#     dcterms:isPartOf nmdc:NamedThing ;
-#     MIXS:0000009 nmdc:GeolocationValue ;
-
-### OWL
-
-# nmdc:SampleTypeEnum a owl:Class,
-#         linkml:EnumDefinition ;
-#     owl:unionOf ( <https://w3id.org/nmdc/SampleTypeEnum#soil> <https://w3id.org/nmdc/SampleTypeEnum#water_extract_soil> ) ;
-#     linkml:permissible_values <https://w3id.org/nmdc/SampleTypeEnum#soil>,
-#         <https://w3id.org/nmdc/SampleTypeEnum#water_extract_soil> .
-
 # Create an RDF graph
 g = Graph()
 
@@ -65,6 +25,39 @@ mat = Namespace("https://w3id.org/nmdc/materialized/")
 mat_obj_prop = mat.ObjectProperty
 mat_range = mat.range
 mat_domain = mat.domain
+
+
+def get_uri_for_element(element_name, schema_view, schema_default_prefix):
+    element_obj = schema_view.get_element(element_name)
+    element_type = type(element_obj).class_name
+    if element_type == 'class_definition':
+        return get_uri_for_class(element_name, schema_view, schema_default_prefix)
+    elif element_type == 'enum_definition':
+        return get_uri_for_enum(element_name, schema_view, schema_default_prefix)
+    elif element_type == 'slot_definition':
+        return get_uri_for_slot(element_name, schema_view, schema_default_prefix)
+    elif element_type == 'type_definition':
+        return get_uri_for_type(element_name, schema_view, schema_default_prefix)
+
+
+def get_uri_for_class(element_name, schema_view, schema_default_prefix):
+    element_obj = schema_view.get_element(element_name)
+    return element_obj.class_uri or f"{schema_default_prefix}:{quote(camelcase(element_name))}"
+
+
+def get_uri_for_enum(element_name, schema_view, schema_default_prefix):
+    element_obj = schema_view.get_element(element_name)
+    return element_obj.enum_uri or f"{schema_default_prefix}:{quote(camelcase(element_name))}"
+
+
+def get_uri_for_slot(element_name, schema_view, schema_default_prefix):
+    element_obj = schema_view.get_element(element_name)
+    return element_obj.slot_uri or f"{schema_default_prefix}:{quote(underscore(element_name))}"
+
+
+def get_uri_for_type(element_name, schema_view, schema_default_prefix):
+    element_obj = schema_view.get_element(element_name)
+    return element_obj.uri or f"{schema_default_prefix}:{quote(underscore(element_name))}"
 
 
 @click.command()
@@ -95,49 +88,26 @@ def cli(schema, output, inc_enums, inc_types, inc_attr_vals):
     }.items():
         g.bind(ns_prefix, ns_uri)  # not Namespace()
 
-    graph_as_curie_dict = []
+    graph_as_curie_dict_list = []
 
     for current_class_name in all_class_names:
         current_class = schema_view.induced_class(current_class_name)
-        # current_class_class_uri = current_class.class_uri or f"{schema_default_prefix}:{current_class_name}"
-        current_class_class_uri = f"{schema_default_prefix}:{camelcase(current_class_name)}"
+        current_class_class_uri = get_uri_for_element(current_class_name, schema_view, schema_default_prefix)
         class_slots = current_class.attributes
         class_slot_names = sorted(s.name for s in class_slots.values())
 
         for current_slot_name in class_slot_names:
-            # current_slot = schema_view.get_slot(current_slot_name)
             current_slot = schema_view.induced_slot(current_slot_name, current_class_name)
-            current_slot_slot_uri = current_slot.slot_uri or f"{schema_default_prefix}:{current_slot_name}"
-            # current_slot_slot_uri = f"{schema_default_prefix}:{underscore(current_slot_name)}"
-            # current_slot_slot_uri = f"mat:{underscore(current_slot_name)}"
+            current_slot_slot_uri = get_uri_for_element(current_slot_name, schema_view, schema_default_prefix)
             current_slot_range = current_slot.range or schema_default_range_name
-            current_slot_range_obj = schema_view.get_element(current_slot_range)
-            current_slot_range_type = type(current_slot_range_obj).class_name
 
-            uri_for_range = None
-            if current_slot_range_type == 'class_definition':
-                current_range_ancestors = schema_view.class_ancestors(current_slot_range)
-                if "AttributeValue" not in current_range_ancestors or inc_attr_vals:
-                    uri_for_range = (
-                        # current_slot_range_obj.class_uri or
-                        # f"{schema_default_prefix}:{quote(camelcase(current_slot_range))}"
-                        f"{schema_default_prefix}:{quote(camelcase(current_slot_range))}"
-                    )
-            elif current_slot_range_type == 'enum_definition' and inc_enums:
-                uri_for_range = (
-                        current_slot_range_obj.enum_uri or
-                        f"{schema_default_prefix}:{quote(camelcase(current_slot_range))}"
-                )
-            elif current_slot_range_type == 'type_definition' and inc_types:
-                uri_for_range = (
-                        current_slot_range_obj.uri or
-                        f"{schema_default_prefix}:{quote(current_slot_range)}"
-                )
-            else:
-                pass
+            uri_for_range = get_uri_for_element(current_slot_range, schema_view, schema_default_prefix)
+
+            print(
+                f"{current_class_name} / {current_class_class_uri} {current_slot_name} / {current_slot_slot_uri} {current_slot_range} / {uri_for_range}")
 
             if uri_for_range:
-                graph_as_curie_dict.append(
+                graph_as_curie_dict_list.append(
                     {'subject': current_class_class_uri, 'predicate': current_slot_slot_uri, 'object': uri_for_range}
                 )
 
@@ -146,15 +116,17 @@ def cli(schema, output, inc_enums, inc_types, inc_attr_vals):
                     any_of_type = type(any_of_object).class_name
                     if any_of_type == "anonymous_slot_expression":
                         if 'range' in any_of_object and any_of_object['range']:
-                            # should go through same logic as above
-                            lazy_object = f"{schema_default_prefix}:{quote(any_of_object['range'])}"
-                            graph_as_curie_dict.append(
+                            lazy_object = get_uri_for_element(any_of_object['range'], schema_view,
+                                                              schema_default_prefix)
+                            graph_as_curie_dict_list.append(
                                 {'subject': current_class_class_uri, 'predicate': current_slot_slot_uri,
                                  'object': lazy_object}
                             )
+                            print(
+                                f"{current_class_name} / {current_class_class_uri} {current_slot_name} / {current_slot_slot_uri} {current_slot_range} / {lazy_object}")
 
     # Print triples with whitespace in any part
-    for triple in graph_as_curie_dict:
+    for triple in graph_as_curie_dict_list:
         if any(' ' in part for part in triple.values()):
             pprint.pprint(triple)
             continue
