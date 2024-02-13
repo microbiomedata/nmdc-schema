cdd96aed48bc1eebfb9d221fb0e1991b45f3c8f9
eecavanna <eecavanna@users.noreply.github.com>
2024-02-13 14:11:39 -0800

    Omit `associated_studies` field when it would be empty

--- nmdc_schema/migrators/migrator_from_X_to_PR53.py
+++ nmdc_schema/migrators/migrator_from_X_to_PR53.py
@@ -18,24 +18,29 @@ class Migrator(MigratorBase):
 
     def move_part_of_to_associated_studies(self, doc: dict):
         r"""
-        Moves `part_of` values that are studies and moves them into a new slot called `associated_studies`
+        Moves `part_of` values that are studies, into a new slot called `associated_studies`
         
         >>> m = Migrator()
         >>> m.move_part_of_to_associated_studies({'id': 123, 'part_of': ['gold:Gs0114663', 'nmdc:sty-55-xxx']})
         {'id': 123, 'associated_studies': ['gold:Gs0114663', 'nmdc:sty-55-xxx']}
+        >>> m.move_part_of_to_associated_studies({'id': 123, 'part_of': []})  # `part_of` list is empty
+        {'id': 123}
         >>> m.move_part_of_to_associated_studies({'id': 123})  # lacks `part_of` key
-        {'id': 123, 'associated_studies': []}
+        {'id': 123}
         """
 
-        # Create and initialize the "associated_studies" field on the document.
-        doc["associated_studies"] = []
-
-        # If the document has a "part_of" field, move its studies to the "associated_studies" field,
-        # then delete the "part_of" field.
+        # Check whether the document has a "part_of" field.
         if "part_of" in doc:
-            studies = doc["part_of"]
-            for study in studies:
-                doc["associated_studies"].append(study)
+
+            # If the "part_of" field contains any study identifiers, add an "associated_studies" list field
+            # to the document and store each study identifier in that list.
+            study_ids = doc["part_of"]
+            if len(study_ids) > 0:
+                doc["associated_studies"] = []
+                for study_id in study_ids:
+                    doc["associated_studies"].append(study_id)
+
+            # Delete the "part_of" field.
             del doc["part_of"]
 
         return doc
