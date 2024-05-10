fc133cd5484510f69b06e8253b088b169cfdb6f3
brynnz22 <brynn.zalmanek@pnnl.gov>
2024-05-10 10:48:14 -0700

    finish up migrator to use difflib SequenceMatcher

--- nmdc_schema/migrators/migrator_from_X_to_PR31.py
+++ nmdc_schema/migrators/migrator_from_X_to_PR31.py
@@ -1,5 +1,6 @@
 from nmdc_schema.migrators.migrator_base import MigratorBase
 from nmdc_schema.migrators.adapters.adapter_base import AdapterBase
+from difflib import SequenceMatcher
 
 class Migrator(MigratorBase):
     r"""
@@ -10,27 +11,6 @@ class Migrator(MigratorBase):
 
     _from_version = "X"
     _to_version = "PR31"
-
-    def __init__(self, adapter: AdapterBase = None, logger=None):
-        r"""
-        Initialize an empty dictionary that maps instrument_name values to their
-        respective omics_processing_set ids
-        """
-
-        # Invoke the `__init__` method of the "parent" class.
-        #
-        # Note: In Python, when a "child" (i.e. inheriting) class doesn't have its own `__init__` method,
-        #       it inherits the "parent" class's `__init__` method. As a result, that inherited `__init__`
-        #       method is the one that Python automatically invokes when the "child" class is instantiated.
-        #
-        #       In contrast, _this_ "child" class _does_ have its own `__init__` method (this comment is in it),
-        #       effectively opting the "child" class out of the aforementioned inheritance and automatic invocation
-        #       of the "parent" class's `__init__` method. So, here, we invoke that method "manually."
-        #
-        super().__init__(adapter=adapter, logger=logger)
-
-        self.omics_processing_instrument_dict = {}
-
 
     def upgrade(self):
         r"""Migrates the database from conforming to the original schema, to conforming to the new schema."""
@@ -54,23 +34,22 @@ class Migrator(MigratorBase):
                 collection_name=collection_name,
                 pipeline=[self.remove_used_slot],
             )
-
-        self.adapter.process_each_document(collection_name="omics_processing_set", pipeline=[self.add_instrument_name])
-
-        for collection_name in workflow_execution_collection_names:
-            self.adapter.process_each_document(
-                collection_name=collection_name,
-                pipeline=[self.remove_used_slot],
-            )
     
+    def preprocess_string(self, s):
+        r"""
+        Removes white spaces, underscores, and hyphens from a string so that SequenceMatcher can
+        find the longest common sequence without disruption by these characters
+        >>> "7T FT-ICR GC-MS"
+        "7TFTICRGCMS"
+        """
+
+        return s.replace(" ", "").replace("_","").replace("-","")
+
     def remove_used_slot(self, doc: dict) -> dict:
         r"""
-        Removes the `used` slot from `WorkflowExecution` subclasses.
-
-        If the value of the `used` slot does not match the value of the `instrument_name` slot of a related `OmicsProcessing`,
-        log the error.
-
-        Or if the `instrument_name` is missing in `OmicsProcessing` but `used` exists in `WorfklowExecution`, add `instrument_name` to `OmicsProcessing`.
+        Removes the used slot from WorkflowExecution subclasses if the value matches the 
+        instrument_name slot from the corresponding OmicsProcessing document by the longest
+        common sequence.
 
         >>> from nmdc_schema.migrators.adapters.dictionary_adapter import DictionaryAdapter
         >>>
@@ -80,43 +59,27 @@ class Migrator(MigratorBase):
         >>> m.remove_used_slot({'id': 'nmdc:metab-123', 'used': 'nmdc:wfc-456', 'was_informed_by': 'nmdc:omcp-123'})
         {'id': 'nmdc:metab-123', 'was_informed_by': 'nmdc:omcp-123'}
         """
+        
         if "used" in doc:
-            try:
-                omics_processing_doc = self.adapter.get_document_having_value_in_field(
-                    collection_name="omics_processing_set", field_name="id", value=doc["was_informed_by"]
-                    )
-
-                if doc["used"] == omics_processing_doc["instrument_name"]:
+            omics_processing_doc = self.adapter.get_document_having_value_in_field(
+                collection_name="omics_processing_set", field_name="id", value=doc["was_informed_by"]
+                )
+            
+            # Preprocess instrument strings to ignore hyphens, underscores, and blank spaces
+            processed_doc = self.preprocess_string(doc["used"])
+            processed_omics_doc = self.preprocess_string(omics_processing_doc["instrument_name"])
+            
+            similarity_ratio = SequenceMatcher(None, processed_doc, processed_omics_doc).ratio()
+            threshold = 0.8
+            if similarity_ratio >= threshold :
+                if similarity_ratio < 1.0:
+                    self.logger.info(f"Workflow with id {doc['id']} has instrument: {doc['used']} matches OmicsProcessing doc instrument: {omics_processing_doc['instrument_name']} well enough")
                     doc.pop("used")
-                elif 'instrument_name' not in omics_processing_doc:
-                    # Get the omics_processing_id from the was_informed_by slot of the workflow execution doc
-                    omics_processing_id = omics_processing_doc["id"]
-                    
-                    if omics_processing_id not in self.omics_processing_instrument_dict:
-                        # add id to dictionary associated with the instrument_name
-                        self.omics_processing_instrument_dict[omics_processing_id] = doc["used"]
-
-            except:
-                self.logger.error(f"WorkflowExecution {doc['id']} used: {doc['used']} does not match OmicsProcessing {omics_processing_doc['id']} instrument_name {omics_processing_doc['instrument_name']}.")
+                elif similarity_ratio == 1.0:
+                    doc.pop("used")
+            else:
+                self.logger.error(f"Workflow doc {doc['id']} with instrument: {doc['used']} does not match {omics_processing_doc['instrument_name']}")
         
         return doc
-
-    def add_instrument_name(self, doc: dict) -> dict:
-        r"""
-        Add `instrument_name` to `OmicsProcessing` set using the `omics_processing_instrument_dict` from the `init`.
-
-        >>> m = Migrator()
-        >>> m.omics_processing_instrument_dict['nmdc:omcp-123'] = 'nmdc:wfc-456'
-        >>> m.add_instrument_name({'id': 'nmdc:omcp-123'})
-        {'id': 'nmdc:omcp-123', 'instrument_name': 'nmdc:wfc-456'}
-        """
-        omics_processing_id = doc['id']
-        doc["instrument_name"] = self.omics_processing_instrument_dict.get(omics_processing_id)
-
-        return doc
-    
-
-    #include a check to match the names usinf difflib
-    # and include the loggers that brynn included in chat
 
     
