81b0e96226c8678483d1fc6ede30ab4cb2047f60
Brynn Zalmanek <46435419+brynnz22@users.noreply.github.com>
2024-05-14 08:54:32 -0700

    Update nmdc_schema/migrators/migrator_from_X_to_PR31.py
    Co-authored-by: eecavanna <134325062+eecavanna@users.noreply.github.com>

--- nmdc_schema/migrators/migrator_from_X_to_PR31.py
+++ nmdc_schema/migrators/migrator_from_X_to_PR31.py
@@ -76,9 +76,7 @@ class Migrator(MigratorBase):
             if similarity_ratio >= threshold:
                 if similarity_ratio < 1.0:
                     self.logger.info(f"Workflow with id {doc['id']} has instrument: {doc['used']} matches OmicsProcessing doc instrument: {omics_processing_doc['instrument_name']} well enough")
-                    doc.pop("used")
-                else:
-                    doc.pop("used")
+                doc.pop("used")
             else:
                 self.logger.error(f"Workflow doc {doc['id']} with instrument: {doc['used']} does not match {omics_processing_doc['instrument_name']}")
         
