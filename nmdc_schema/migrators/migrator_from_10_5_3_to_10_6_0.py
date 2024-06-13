92f53904a6fc898aba3ddae056946617329214a6
eecavanna <eecavanna@users.noreply.github.com>
2024-06-13 16:48:20 -0700

    Format code

--- nmdc_schema/migrators/migrator_from_10_5_3_to_10_6_0.py
+++ nmdc_schema/migrators/migrator_from_10_5_3_to_10_6_0.py
@@ -4,8 +4,7 @@ from nmdc_schema.migrators.migrator_base import MigratorBase
 
 
 class Migrator(MigratorBase):
-    r"""Migrates a database between two schemas.
-    """
+    r"""Migrates a database between two schemas."""
 
     _from_version = "10.5.3"
     _to_version = "10.6.0"
@@ -14,15 +13,21 @@ class Migrator(MigratorBase):
     #       and getting this pattern from the file `project/nmdc_materialized_patterns.yaml`.
     #       Reference: https://github.com/microbiomedata/nmdc-schema/pull/2059#discussion_r1639005255
     #
-    pattern = re.compile(r"(^(nmdc):wfnom-([0-9][a-z]{0,6}[0-9])-([A-Za-z0-9]{1,})(\.[0-9]{1,})$)")
+    pattern = re.compile(
+        r"(^(nmdc):wfnom-([0-9][a-z]{0,6}[0-9])-([A-Za-z0-9]{1,})(\.[0-9]{1,})$)"
+    )
 
     def upgrade(self) -> None:
         r"""Migrates the database from conforming to the original schema, to conforming to the new schema."""
 
         self.adapter.process_each_document(
-            "nom_analysis_activity_set", [self.populate_alternative_identifiers,self.update_nom_analysis_activity_id]
+            "nom_analysis_activity_set",
+            [
+                self.populate_alternative_identifiers,
+                self.update_nom_analysis_activity_id,
+            ],
         )
-    
+
     def is_valid_id(self, s: str) -> bool:
         r"""
         Helper function that checks whether a string is a valid ID (has a version number).
@@ -39,7 +44,6 @@ class Migrator(MigratorBase):
 
         return self.pattern.search(s) is not None
 
-
     def populate_alternative_identifiers(self, nom_analysis_activity: dict) -> dict:
         r"""
         Checks to see if a nom_analysis_activity has any alternative_identifiers. If there are any, then the ID is added if it's invalid.
@@ -48,15 +52,18 @@ class Migrator(MigratorBase):
         >>> m = Migrator()
         >>> m.populate_alternative_identifiers({'id': 'nmdc:wfnom-13-7yf9qj85'}) # test: adds alternative identifiers
         {'id': 'nmdc:wfnom-13-7yf9qj85', 'alternative_identifiers': ['nmdc:wfnom-13-7yf9qj85']}
-        >>> m.populate_alternative_identifiers({'id': 'nmdc:wfnom-13-7yf9qj85.1','alternative_identifiers': ['alt 1']}) # test no change, alternative identifiers already present with valid id
+        >>> m.populate_alternative_identifiers({'id': 'nmdc:wfnom-13-7yf9qj85.1',
+        ...                                     'alternative_identifiers': ['alt 1']}) # test no change, alternative identifiers already present with valid id
         {'id': 'nmdc:wfnom-13-7yf9qj85.1', 'alternative_identifiers': ['alt 1']}
         >>> m.populate_alternative_identifiers({'id': 'nmdc:wfnom-13-7yf9qj85.1'}) # test no change, alternative identifiers not present, but with valid id
         {'id': 'nmdc:wfnom-13-7yf9qj85.1'}
 
         """
-        self.logger.info(f"Checking alternative_identifiers for NomAnalysisActivity: {nom_analysis_activity['id']}")
+        self.logger.info(
+            f"Checking alternative_identifiers for NomAnalysisActivity: {nom_analysis_activity['id']}"
+        )
 
-        nom_id = nom_analysis_activity['id']
+        nom_id = nom_analysis_activity["id"]
         if not self.is_valid_id(nom_id):
             if "alternative_identifiers" not in nom_analysis_activity.keys():
                 nom_analysis_activity["alternative_identifiers"] = []
@@ -76,10 +83,12 @@ class Migrator(MigratorBase):
         {'id': 'nmdc:wfnom-13-7yf9qj85.1'}
 
         """
-        self.logger.info(f"Checking ID for NomAnalysisActivity: {nom_analysis_activity['id']}")
+        self.logger.info(
+            f"Checking ID for NomAnalysisActivity: {nom_analysis_activity['id']}"
+        )
 
         # If there's no version number, add one
         if not self.is_valid_id(nom_analysis_activity["id"]):
-            nom_analysis_activity['id'] = str(nom_analysis_activity['id']) + ".1"
+            nom_analysis_activity["id"] = str(nom_analysis_activity["id"]) + ".1"
 
         return nom_analysis_activity
