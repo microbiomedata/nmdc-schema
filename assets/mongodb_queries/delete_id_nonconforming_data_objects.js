c99072de1f6765959c75b469b5ebe2dc88bc9012
aclum <aclum@lbl.gov>
2024-06-17 15:23:45 -0700

    updating legacy data object query to find command instead of delete

--- assets/mongodb_queries/delete_id_nonconforming_data_objects.js
+++
@@ -1,9 +0,0 @@
-db.getCollection("data_object_set").find({
-    "id": {
-        "$not": /^nmdc:dobj/
-    }
-}).forEach(function (doc) {
-    db.data_object_set.deleteOne({
-        "_id": doc._id
-    });
-});
