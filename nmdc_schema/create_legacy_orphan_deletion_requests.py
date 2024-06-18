745d45a44fc43ffe0511465a4ea585ac761dd44a
aclum <aclum@lbl.gov>
2024-06-18 16:38:04 -0700

    updating section on data objects to handle legacy IDs with newlines

--- nmdc_schema/create_legacy_orphan_deletion_requests.py
+++ nmdc_schema/create_legacy_orphan_deletion_requests.py
@@ -7,7 +7,7 @@ def gen_delete(delete_list: list, key: str, limit: int, del_coll: str):
         raise ValueError("limit must be 0 or 1.")
     delete_statement = []
     for record in delete_list:
-        delete_statement.append({"q": {key: record}, "limit": limit})
+        delete_statement.append({"q": {key: r"%s" % record}, "limit": limit})
     request_body_json = {"delete": del_coll, "deletes": delete_statement}
     request_body_file = del_coll + "_request_body.json"
     with open(request_body_file, "w") as f:
@@ -24,10 +24,16 @@ del_coll = "functional_annotation_agg"
 gen_delete(delete_agg_list, "metagenome_annotation_id", 0, del_coll)
 
 # delete leagacy orphan data_object_set records
+#uses regex as a workaround for newline issues double backslash issues
 delete_do_list = []
 with open("legacy_orphan.nmdc.data_object_set.csv", newline="") as csvfile:
     do_records = csv.reader(csvfile, delimiter=",")
     for row in do_records:
-        delete_do_list.append(row[0])
-del_coll = "data_object_set"
-gen_delete(delete_do_list, "id", 1, del_coll)
+        strip_id=row[1].replace("\\n", "")
+        print(strip_id)
+        delete_do_list.append({"q":{"id":{"$regex":strip_id}}, "limit": 1})
+    request_body_json = {"delete": "data_object_set", "deletes": delete_do_list}
+    request_body_file = "data_object_set_request_body.json"
+    with open(request_body_file, "w") as f:
+        json.dump(request_body_json, f)
+    
