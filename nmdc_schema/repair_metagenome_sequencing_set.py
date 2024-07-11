7555e70b1a7eba903e85ed660586a2e727968ff1
aclum <aclum@lbl.gov>
2024-07-11 16:02:42 -0700

    making sure local and remote are the same

---
+++ nmdc_schema/repair_metagenome_sequencing_set.py
@@ -0,0 +1,18 @@
+import json
+update_list=[]
+
+file= open ('/Users/aclum/Downloads/nmdc.metagenome_sequencing_activity_set.csv','r')
+
+for i in file.readlines():
+   i=i.strip()
+   value_array=i.split(',')
+   update_list.append({"q": {"id": value_array[1] }, "u": {"$set": {"has_input": [value_array[2]]}}})
+
+#print(update_list)
+json_body=({
+    "update": "metagenome_sequencing_activity_set",
+    "updates": update_list})
+
+with open ("mg_seq_repair_inputs.json",'w') as f:
+  json.dump(json_body,f)
+
