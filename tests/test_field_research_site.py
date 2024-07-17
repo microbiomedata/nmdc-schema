d4b37a9f05a7788ff01e7c7c9877f23ce6bff302
Mark A. Miller <MAM@lbl.gov>
2024-07-17 15:26:19 -0400

    one line frs instantiation

--- tests/test_field_research_site.py
+++ tests/test_field_research_site.py
@@ -5,9 +5,7 @@ from nmdc_schema.nmdc import FieldResearchSite
 
 class TestBiosampleInstantiation(unittest.TestCase):
     def test_with_part_of(self):
-        frs = FieldResearchSite(
-            id="frs:1",
-        )
+        frs = FieldResearchSite(id="frs:1", )
         self.assertIsNotNone(frs)
 
 
