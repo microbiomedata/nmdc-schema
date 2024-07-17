fd65049906efc22b9287c9d7c4129154e5ada8fc
Mark A. Miller <MAM@lbl.gov>
2024-07-17 15:25:01 -0400

    remove temporary fixes

--- tests/test_field_research_site.py
+++ tests/test_field_research_site.py
@@ -1,16 +1,12 @@
 import unittest
 
-from nmdc_schema.nmdc import FieldResearchSite, TextValue, GeolocationValue
+from nmdc_schema.nmdc import FieldResearchSite
 
 
 class TestBiosampleInstantiation(unittest.TestCase):
     def test_with_part_of(self):
         frs = FieldResearchSite(
             id="frs:1",
-            geo_loc_name=TextValue(has_raw_value="USA, California, Stockton"),
-            lat_lon=GeolocationValue(
-                latitude=37.9577,
-                longitude=-121.2908)
         )
         self.assertIsNotNone(frs)
 
