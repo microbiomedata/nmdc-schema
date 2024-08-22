ecaefcf0e9fecfc2c5290736ed0d35e7024ce21e
Mark A. Miller <MAM@lbl.gov>
2024-08-22 09:41:06 -0400

    add nmdc_schema/nmdc.py; no new content

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-08-20T16:07:33
+# Generation date: 2024-08-22T09:37:18
 # Schema: NMDC
 #
 # id: https://w3id.org/nmdc/nmdc
