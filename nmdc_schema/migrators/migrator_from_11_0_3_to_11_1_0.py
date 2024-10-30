513cf6e03f7f617aa2816daa454b017b872a5060
brynnz22 <brynn.zalmanek@pnnl.gov>
2024-10-30 14:48:28 -0700

    implement partials

--- nmdc_schema/migrators/migrator_from_11_0_3_to_11_1_0.py
+++ nmdc_schema/migrators/migrator_from_11_0_3_to_11_1_0.py
@@ -1,17 +1,36 @@
 from nmdc_schema.migrators.migrator_base import MigratorBase
+from nmdc_schema.migrators.partials.migrator_from_11_0_3_to_11_1_0 import (
+    get_migrator_classes,
+)
 
 
 class Migrator(MigratorBase):
     r"""
     Migrates a database between two schemas.
 
-    Note: This is a "no op" migrator. Its existence serves as documentation that no
-          database migration is necessary between the specified schema versions.
+    Specifically, this migrator migrates a database that conforms to the "pre-Berkeley schema"
+    into one that conforms to the "Berkeley schema".
+
+    Reference: https://pypi.org/project/nmdc-schema/#history
     """
 
     _from_version = "11.0.3"
-    _to_version = "11.1.0"
+    _to_version = "11.1.0" 
 
     def upgrade(self) -> None:
-        r"""Do nothing."""
-        pass
+        r"""
+        Migrates the database from conforming to the original schema, to conforming to the new schema.
+
+        This migrator uses partial migrators. It runs them in the order in which they were designed to be run.
+        """
+
+        migrator_classes = get_migrator_classes()
+        num_migrators = len(migrator_classes)
+        for idx, migrator_class in enumerate(migrator_classes):
+            self.logger.info(f"Running migrator {idx + 1} of {num_migrators}")
+            self.logger.debug(
+                f"Migrating from {migrator_class.get_origin_version()} "
+                f"to {migrator_class.get_destination_version()}"
+            )
+            migrator = migrator_class(adapter=self.adapter, logger=self.logger)
+            migrator.upgrade()
