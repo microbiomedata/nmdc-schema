8b23add7d34865d0996aebc594e3fcba26968786
eecavanna <eecavanna@users.noreply.github.com>
2025-03-10 22:00:02 -0700

    Add transformation function to handle `storage_process_set` documents

--- nmdc_schema/migrators/migrator_from_11_4_0_to_11_5_0.py
+++ nmdc_schema/migrators/migrator_from_11_4_0_to_11_5_0.py
@@ -19,19 +19,19 @@ class Migrator(MigratorBase):
         ----------------------------
         In a database conforming to schema 11.4.0, instances of the `PortionOfSubstance` class can reside in
         the multivalued `substances_used` field of instances of the following schema classes:
-        - `Extraction` (https://microbiomedata.github.io/nmdc-schema/Extraction/)
+        - [x] `Extraction` (https://microbiomedata.github.io/nmdc-schema/Extraction/)
            - All of these are documents in the `material_processing_set` collection,
              having a type value of `nmdc:Extraction`.
-        - `StorageProcess` (https://microbiomedata.github.io/nmdc-schema/StorageProcess/)
+        - [x] `StorageProcess` (https://microbiomedata.github.io/nmdc-schema/StorageProcess/)
            - All of these are documents in the `storage_process_set` collection,
              having a type value of `nmdc:StorageProcess`.
-        - `DissolvingProcess` (https://microbiomedata.github.io/nmdc-schema/DissolvingProcess/)
+        - [x] `DissolvingProcess` (https://microbiomedata.github.io/nmdc-schema/DissolvingProcess/)
           - All of these are documents in the `material_processing_set` collection,
             having a type value of `nmdc:DissolvingProcess`.
-        - `ChemicalConversionProcess` (https://microbiomedata.github.io/nmdc-schema/ChemicalConversionProcess/)
+        - [x] `ChemicalConversionProcess` (https://microbiomedata.github.io/nmdc-schema/ChemicalConversionProcess/)
           - All of these are documents in the `material_processing_set` collection,
             having a type value of `nmdc:ChemicalConversionProcess`.
-        - `MobilePhaseSegment` (https://microbiomedata.github.io/nmdc-schema/MobilePhaseSegment/)
+        - [ ] `MobilePhaseSegment` (https://microbiomedata.github.io/nmdc-schema/MobilePhaseSegment/)
           - These are not represented by entire documents in any collection. Instead,
             they are represented by objects having a type value of `MobilePhaseSegment`
             in the multivalued `ordered_mobile_phases` fields of instances of the following schema classes:
@@ -46,6 +46,39 @@ class Migrator(MigratorBase):
         self.adapter.process_each_document("material_processing_set", [
             self.remove_sample_state_information_field_within_material_processing
         ])
+
+        self.adapter.process_each_document("storage_process_set", [
+            self.remove_sample_state_information_field_within_storage_process
+        ])
+
+    def remove_sample_state_information_field_within_storage_process(self, storage_process: dict) -> dict:
+        r"""
+        If the specified document has a type value of `nmdc:StorageProcess`, remove the
+        `sample_state_information` field from any `PortionOfSubstance` instances that are in the
+        document's `substances_used` field.
+
+        >>> m = Migrator()
+
+        # Test: Removes the `sample_state_information` field from multiple `substances_used` dictionaries.
+        >>> m.remove_sample_state_information_field_within_storage_process({
+        ...     'id': 123,
+        ...     'type': 'nmdc:StorageProcess',
+        ...     'substances_used': [
+        ...         {'type': 'nmdc:PortionOfSubstance', 'sample_state_information': 'solid', 'substance_role': 'base'},
+        ...         {'type': 'nmdc:PortionOfSubstance', 'sample_state_information': 'gas', 'substance_role': 'acid'},
+        ...     ]
+        ... })
+        {'id': 123, 'type': 'nmdc:StorageProcess', 'substances_used': [{'type': 'nmdc:PortionOfSubstance', 'substance_role': 'base'}, {'type': 'nmdc:PortionOfSubstance', 'substance_role': 'acid'}]}
+        """
+
+        target_document_type = "nmdc:StorageProcess"
+        if storage_process.get("type", None) == target_document_type:
+            substances_used = storage_process.get("substances_used", [])  # `substances_used` is multivalued
+            for substance_used in substances_used:
+                if substance_used.get("type") == "nmdc:PortionOfSubstance":
+                    substance_used.pop("sample_state_information", None)  # deletes the key if it is present
+
+        return storage_process
 
     def remove_sample_state_information_field_within_material_processing(self, material_processing: dict) -> dict:
         r"""
