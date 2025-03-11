fb90f193932c39b8469e65bfd7aec4a142b9192d
eecavanna <eecavanna@users.noreply.github.com>
2025-03-11 12:11:09 -0700

    Refine comment format

--- nmdc_schema/migrators/migrator_from_11_4_0_to_11_5_0.py
+++ nmdc_schema/migrators/migrator_from_11_4_0_to_11_5_0.py
@@ -11,36 +11,41 @@ class Migrator(MigratorBase):
         r"""
         Migrates the database from conforming to the original schema, to conforming to the new schema.
 
-        Summary of schema change
-        ------------------------
-        Slot `sample_state_information` was removed from schema class `PortionOfSubstance`.
+        Between schema version 11.4.0 and 11.5.0, the slot `sample_state_information` was removed from the schema class
+        `PortionOfSubstance`. This function updates all instances of that schema class in the database accordingly.
 
-        Notes while implementing this migrator
-        --------------------------------------
-        In a database conforming to schema 11.4.0, instances of the `PortionOfSubstance` class can reside in
-        the multivalued `substances_used` field of instances of the following schema classes:
-        - [x] `Extraction` (https://microbiomedata.github.io/nmdc-schema/Extraction/)
-           - All of these are documents in the `material_processing_set` collection,
-             having a type value of `nmdc:Extraction`.
-        - [x] `StorageProcess` (https://microbiomedata.github.io/nmdc-schema/StorageProcess/)
-           - All of these are documents in the `storage_process_set` collection,
-             having a type value of `nmdc:StorageProcess`.
-        - [x] `DissolvingProcess` (https://microbiomedata.github.io/nmdc-schema/DissolvingProcess/)
-          - All of these are documents in the `material_processing_set` collection,
-            having a type value of `nmdc:DissolvingProcess`.
-        - [x] `ChemicalConversionProcess` (https://microbiomedata.github.io/nmdc-schema/ChemicalConversionProcess/)
-          - All of these are documents in the `material_processing_set` collection,
-            having a type value of `nmdc:ChemicalConversionProcess`.
-        - [x] `MobilePhaseSegment` (https://microbiomedata.github.io/nmdc-schema/MobilePhaseSegment/)
-          - These are not represented by entire documents in any collection. Instead,
-            they are represented by objects having a type value of `MobilePhaseSegment`
-            in the multivalued `ordered_mobile_phases` fields of instances of the following schema classes:
-            - [x] `ChromatographyConfiguration` (https://microbiomedata.github.io/nmdc-schema/ChromatographyConfiguration/)
-              - All of these are documents in the `configuration_set` collection,
-                having a type value of `nmdc:ChromatographyConfiguration`.
-            - [x] `ChromatographicSeparationProcess` (https://microbiomedata.github.io/nmdc-schema/ChromatographicSeparationProcess/)
-              - All of these are documents in the `material_processing_set` collection,
-                having a type value of `nmdc:ChromatographicSeparationProcess`.
+        Note: In a database conforming to schema 11.4.0, instances of the `PortionOfSubstance` class cannot exist
+              as documents in a collection. Instead, they can only exist as objects in the multivalued `substances_used`
+              field of instances of the specific schema classes. Those schema classes are:
+              - [x] 1. `Extraction`
+                - Docs: https://microbiomedata.github.io/nmdc-schema/Extraction/
+                - All of these are documents in the `material_processing_set` collection,
+                  having a type value of `nmdc:Extraction`.
+              - [x] 2. `StorageProcess`
+                - Docs: https://microbiomedata.github.io/nmdc-schema/StorageProcess/
+                - All of these are documents in the `storage_process_set` collection,
+                  having a type value of `nmdc:StorageProcess`.
+              - [x] 3. `DissolvingProcess`
+                - Docs: https://microbiomedata.github.io/nmdc-schema/DissolvingProcess/
+                - All of these are documents in the `material_processing_set` collection,
+                  having a type value of `nmdc:DissolvingProcess`.
+              - [x] 4. `ChemicalConversionProcess`
+                - Docs: https://microbiomedata.github.io/nmdc-schema/ChemicalConversionProcess/
+                - All of these are documents in the `material_processing_set` collection,
+                  having a type value of `nmdc:ChemicalConversionProcess`.
+              - [x] 5. `MobilePhaseSegment`
+                - Docs: https://microbiomedata.github.io/nmdc-schema/MobilePhaseSegment/
+                - These are not represented by entire documents in any collection. Instead,
+                  they are represented by objects having a type value of `MobilePhaseSegment`
+                  in the multivalued `ordered_mobile_phases` fields of instances of the following schema classes:
+                  - [x] A. `ChromatographyConfiguration`
+                    - Docs: https://microbiomedata.github.io/nmdc-schema/ChromatographyConfiguration/
+                    - All of these are documents in the `configuration_set` collection,
+                      having a type value of `nmdc:ChromatographyConfiguration`.
+                  - [x] B. `ChromatographicSeparationProcess`
+                    - Docs: https://microbiomedata.github.io/nmdc-schema/ChromatographicSeparationProcess/
+                    - All of these are documents in the `material_processing_set` collection,
+                      having a type value of `nmdc:ChromatographicSeparationProcess`.
         """
 
         self.adapter.process_each_document("material_processing_set", [
