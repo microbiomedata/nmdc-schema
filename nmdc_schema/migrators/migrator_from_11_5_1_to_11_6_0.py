54c6c6bd942e4fdf133ea52f6bb2b0dd92f6e922
Patrick Kalita <pkalita@lbl.gov>
2025-04-11 14:28:29 -0700

    Also check for data_objects associated with workflow_execution instances

--- nmdc_schema/migrators/migrator_from_11_5_1_to_11_6_0.py
+++ nmdc_schema/migrators/migrator_from_11_5_1_to_11_6_0.py
@@ -11,6 +11,7 @@ class Migrator(MigratorBase):
     def __init__(self, adapter: AdapterBase=None, logger=None):
         super().__init__(adapter, logger)
 
+        self.ids_of_metatranscriptome_data_generation_with_no_output = set()
         self.ids_of_data_objects_to_update = set()
         self.data_object_type_update_map = {
             "Metagenome Raw Reads": "Metatranscriptome Raw Reads",
@@ -20,29 +21,75 @@ class Migrator(MigratorBase):
 
     def upgrade(self) -> None:
         r"""Migrates the database from conforming to the original schema, to conforming to the new schema."""
-        self.adapter.do_for_each_document("data_generation_set", self.find_metatranscriptome_data_objects)
+
+        self.ids_of_metatranscriptome_data_generation_with_no_output = set()
+        self.ids_of_data_objects_to_update = set()
+
+        self.adapter.do_for_each_document("data_generation_set", self.scan_data_generation_set)
+        self.adapter.do_for_each_document("workflow_execution_set", self.scan_workflow_execution_set)
         self.logger.info(f"Found {len(self.ids_of_data_objects_to_update)} data objects to update.")
 
         self.adapter.process_each_document("data_object_set",[self.update_data_object_type])
 
-    def find_metatranscriptome_data_objects(self, data_generation: dict) -> None:
+    def scan_data_generation_set(self, data_generation: dict) -> None:
         r"""
         If the incoming data_generation object has analyte_category set to "metatranscriptome", and
-        it has_output a data_object with data_object_type set to "Metagenome Raw Reads", then
-        add the data_object to the set of ids_of_data_objects_to_update.
+        has_output is not set, add the id of the data_generation object to the set of
+        ids_of_metatranscriptome_data_generation_with_no_output. This set will be used when scanning
+        the workflow_execution_set. If the incoming data_generation object has analyte_category set
+        to "metatranscriptome", and has_output is set, add each data_generation id in the has_output
+        list to the set of ids_of_data_objects_to_update.
 
         >>> m = Migrator()
-        >>> m.find_metatranscriptome_data_objects({'id': 'nmdc:dgns-11-12345678', 'analyte_category': 'metatranscriptome', 'has_output': ["nmdc:dobj-11-abcd1234"]})
+        >>> m.scan_data_generation_set({'id': 'nmdc:dgns-11-12345678', 'analyte_category': 'metatranscriptome', 'has_output': ["nmdc:dobj-11-abcd1234"]})
         >>> m.ids_of_data_objects_to_update
         {'nmdc:dobj-11-abcd1234'}
-        >>> m.find_metatranscriptome_data_objects({'id': 'nmdc:dgns-11-12345678', 'analyte_category': 'metagenome', 'has_output': ["nmdc:dobj-11-abcd1234"]})
+        >>> m.ids_of_metatranscriptome_data_generation_with_no_output
+        set()
+        >>> m = Migrator()
+        >>> m.scan_data_generation_set({'id': 'nmdc:dgns-11-12345678', 'analyte_category': 'metagenome', 'has_output': ["nmdc:dobj-11-abcd1234"]})
+        >>> m.ids_of_data_objects_to_update
+        set()
+        >>> m.ids_of_metatranscriptome_data_generation_with_no_output
+        set()
+        >>> m = Migrator()
+        >>> m.scan_data_generation_set({'id': 'nmdc:dgns-11-12345678', 'analyte_category': 'metatranscriptome'})
+        >>> m.ids_of_data_objects_to_update
+        set()
+        >>> m.ids_of_metatranscriptome_data_generation_with_no_output
+        {'nmdc:dgns-11-12345678'}
+        """
+        if data_generation.get("analyte_category") == "metatranscriptome":
+            if "has_output" not in data_generation:
+                self.ids_of_metatranscriptome_data_generation_with_no_output.add(data_generation["id"])
+            else:
+                for output in data_generation.get("has_output", []):
+                    self.ids_of_data_objects_to_update.add(output)
+
+    def scan_workflow_execution_set(self, workflow_execution: dict) -> None:
+        r"""
+        If the incoming workflow_execution object has was_informed_by relationship to a
+        data_generation object that was flagged in the scan_data_generation_set step (i.e. it is
+        in the ids_of_metatranscriptome_data_generation_with_no_output set), and the
+        workflow_execution object has a has_output field, add each data_generation id in the
+        has_output list to the set of ids_of_data_objects_to_update.
+
+        >>> m = Migrator()
+        >>> m.ids_of_metatranscriptome_data_generation_with_no_output = {'nmdc:dgns-11-12345678'}
+        >>> m.scan_workflow_execution_set({'id': 'nmdc:wfnom-11-12345678', 'was_informed_by': 'nmdc:dgns-11-12345678', 'has_output': ["nmdc:dobj-11-abcd1234"]})
+        >>> m.ids_of_data_objects_to_update
+        {'nmdc:dobj-11-abcd1234'}
+        >>> m = Migrator()
+        >>> m.ids_of_metatranscriptome_data_generation_with_no_output = {'nmdc:dgns-11-12345678'}
+        >>> m.scan_workflow_execution_set({'id': 'nmdc:wfnom-11-12345678', 'was_informed_by': 'nmdc:dgns-11-98765432', 'has_output': ["nmdc:dobj-11-abcd1234"]})
         >>> m.ids_of_data_objects_to_update
         set()
         """
-        self.ids_of_data_objects_to_update = set()
-        if data_generation.get("analyte_category") == "metatranscriptome":
-            for output in data_generation.get("has_output", []):
-                self.ids_of_data_objects_to_update.add(output)
+        if "was_informed_by" in workflow_execution:
+            data_generation_id = workflow_execution["was_informed_by"]
+            if data_generation_id in self.ids_of_metatranscriptome_data_generation_with_no_output:
+                for output in workflow_execution.get("has_output", []):
+                    self.ids_of_data_objects_to_update.add(output)
 
     def update_data_object_type(self, data_object: dict) -> dict:
         r"""
@@ -54,7 +101,7 @@ class Migrator(MigratorBase):
 
         >>> m = Migrator()
 
-        # In reality, this would be set by the find_metatranscriptome_data_objects method
+        # In reality, this would be set by scan_data_generation_set and scan_workflow_execution_set
         >>> m.ids_of_data_objects_to_update = {"nmdc:dobj-11-abcd1234"}
 
         >>> m.update_data_object_type({'id': 'nmdc:dobj-11-abcd1234', 'data_object_type': 'Metagenome Raw Reads'})
