61c988a8c2cda296fade8253346e40a060f4a528
Patrick Kalita <pkalita@lbl.gov>
2025-04-17 12:22:01 -0700

    Add test for making a Database instance in Python

---
+++ tests/test_database.py
@@ -0,0 +1,11 @@
+import unittest
+
+from nmdc_schema.nmdc import Database
+
+class TestDatabase(unittest.TestCase):
+    def test_database(self):
+        """Test that a nmdc:Database instance can be created."""
+        database = Database()
+
+        # Not much to assert here; this is mainly a smoke test
+        assert database.biosample_set == []
