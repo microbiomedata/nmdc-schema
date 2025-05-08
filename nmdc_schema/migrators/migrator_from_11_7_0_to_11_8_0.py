65cd49bde18b2a35d388b27c65ac0ca0cb10a7bd
olivia.hess <olivia.hess@pnnl.gov>
2025-05-08 15:44:55 -0400

    changing to use do_for_each_document

--- nmdc_schema/migrators/migrator_from_11_7_0_to_11_8_0.py
+++ nmdc_schema/migrators/migrator_from_11_7_0_to_11_8_0.py
@@ -8,16 +8,15 @@ class Migrator(MigratorBase):
 
     def upgrade(self) -> None:
         r"""Migrates the database from conforming to the original schema, to conforming to the new schema."""
-        self.adapter.process_each_document("data_object_set", [self.validate_data_object_type])
+        self.adapter.do_for_each_document("data_object_set", self.validate_data_object_type)
 
-    def validate_data_object_type(self, data_object: dict) -> dict:
+    def validate_data_object_type(self, data_object: dict) -> None:
         r"""
         Raises an exception if the document either lacks a `data_object_type`
         field or it has a `data_object_type` field whose value is falsey.
 
         >>> m = Migrator()
         >>> m.validate_data_object_type({"id": 123, "type": "nmdc:DataObject", "data_object_type": "Type"})
-        {'id': 123, 'type': 'nmdc:DataObject', 'data_object_type': 'Type'}
         >>> m.validate_data_object_type({"id": 123, "type": "nmdc:DataObject"})
         Traceback (most recent call last):
         ...
@@ -25,4 +24,3 @@ class Migrator(MigratorBase):
         """
         if not data_object.get("data_object_type"):
             raise ValueError(f"data_object_type is required and is not present in the data object {data_object.get('id')}")
-        return data_object
