0f69772facbc71d7ecf7cb04158a275e82d8de9e
olivia.hess <olivia.hess@pnnl.gov>
2025-05-21 10:26:36 -0400

    update to raise error if unexpected value

--- nmdc_schema/migrators/partials/migrator_from_11_7_0_to_11_8_0/migrator_from_11_7_0_to_11_8_0_part_2.py
+++ nmdc_schema/migrators/partials/migrator_from_11_7_0_to_11_8_0/migrator_from_11_7_0_to_11_8_0_part_2.py
@@ -14,13 +14,20 @@ class Migrator(MigratorBase):
     def clarify_ft_data_category(self, data_object: dict) -> dict:
         r"""
         If the data object does not have data_category field, add a value based on the data_object_type.
-        If the data object has any other value for data_object_type; ignore it.
+        If the data_category field is already present, do nothing.
+        A mapping is loaded from a YAML file. The mapping is a dictionary where the keys are data_object_type
+        and the values are data_category.
+        If the data_object_type is not in the mapping, raise a ValueError.
 
         >>> m = Migrator()
         >>> m.clarify_ft_data_category({"id":123, "type":"nmdc:DataObject", "data_object_type":"Metagenome Raw Reads"})
         {'id': 123, 'type': 'nmdc:DataObject', 'data_object_type': 'Metagenome Raw Reads', 'data_category': 'instrument_data'}
         >>> m.clarify_ft_data_category({"id":123, "type":"nmdc:DataObject", "data_category": "instrument_data"})
         {'id': 123, 'type': 'nmdc:DataObject', 'data_category': 'instrument_data'}
+        >>> m.clarify_ft_data_category({"id":123, "type":"nmdc:DataObject", "data_object_type": "Metag Raw"})
+        Traceback (most recent call last):
+        ...
+        ValueError: data_object_type Metag Raw is not supported for data_category assignment
         """
         data_category_map = load_yaml_asset('migrator_from_11_7_to_11_8/data_category_map.yaml')
 
@@ -28,5 +35,7 @@ class Migrator(MigratorBase):
             try:
                 data_object["data_category"] = data_category_map[data_object.get("data_object_type")]
             except KeyError:
-                data_object["data_category"] = ""
+                raise ValueError(
+                    f"data_object_type {data_object.get('data_object_type')} is not found in the mapping. Cannot assign data_category."
+                )
         return data_object
