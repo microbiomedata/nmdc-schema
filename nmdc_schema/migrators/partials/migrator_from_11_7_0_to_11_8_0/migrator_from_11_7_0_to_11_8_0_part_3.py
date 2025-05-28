db28448b45943b55b1a7c57ba411eddcd2033b7f
Olivia Hess <olivia.hess@pnnl.gov>
2025-05-28 16:50:14 -0400

    Update nmdc_schema/migrators/partials/migrator_from_11_7_0_to_11_8_0/migrator_from_11_7_0_to_11_8_0_part_3.py
    Co-authored-by: eecavanna <134325062+eecavanna@users.noreply.github.com>

--- nmdc_schema/migrators/partials/migrator_from_11_7_0_to_11_8_0/migrator_from_11_7_0_to_11_8_0_part_3.py
+++ nmdc_schema/migrators/partials/migrator_from_11_7_0_to_11_8_0/migrator_from_11_7_0_to_11_8_0_part_3.py
@@ -13,7 +13,7 @@ class Migrator(MigratorBase):
 
     def validate_mass_spec_slots(self, data_generation_record: dict) -> None:
         r"""
-        If the data object does not have eluent_introduction_category or has_mass_spectrometry_configuration AND those keys do not have a value, raise a ValueError.
+        If the data object is of type `nmdc:MassSpectrometry` and either (a) its `eluent_introduction_category` field is missing or consists of `None`, or (b) its `has_mass_spectrometry_configuration` field is missing or consists of `None`, raise a `ValueError`.
 
         >>> m = Migrator()
         >>> m.validate_mass_spec_slots({"id": 123, "type": "nmdc:MassSpectrometry", "eluent_introduction_category": "gas_chromatography", "has_mass_spectrometry_configuration": "nmdc:mscon-123"})
