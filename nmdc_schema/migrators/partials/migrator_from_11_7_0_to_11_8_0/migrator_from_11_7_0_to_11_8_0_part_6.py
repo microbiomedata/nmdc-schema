8c951ca6671c0ad55490a4214634593257ac8e2c
olivia.hess <olivia.hess@pnnl.gov>
2025-06-20 12:15:52 -0400

    function name updates

--- nmdc_schema/migrators/partials/migrator_from_11_7_0_to_11_8_0/migrator_from_11_7_0_to_11_8_0_part_6.py
+++ nmdc_schema/migrators/partials/migrator_from_11_7_0_to_11_8_0/migrator_from_11_7_0_to_11_8_0_part_6.py
@@ -26,9 +26,9 @@ class Migrator(MigratorBase):
         - the 'was_generated_by' field in the DataObjects will be updated to point to the corresponding DataGeneration records.
 
         """
-        self.adapter.do_for_each_document("workflow_execution_set", self.store_we_ms_feilds)
+        self.adapter.do_for_each_document("workflow_execution_set", self.store_we_ms_fields)
         self.adapter.process_each_document("data_object_set", [self.link_do_to_dg])
-        self.adapter.process_each_document("data_generation_set", [self.migrate_dg_fields])
+        self.adapter.process_each_document("data_generation_set", [self.migrate_fields_to_dg])
 
     def store_we_ms_fields(self, workflow_execution_record: dict) -> None:
         r"""
