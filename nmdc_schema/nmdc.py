d23ce8c8a5a7765d64978a3bf85fd0b41e6c214e
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-07-24 10:48:54 -0700

    stashing for main branch merge

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-07-18T15:27:12
+# Generation date: 2025-07-19T17:30:28
 # Schema: NMDC
 #
 # id: https://w3id.org/nmdc/nmdc
