7d19c4a59af47570ed9ad18f973c40c3d0e4b166
Patrick Kalita <pkalita@lbl.gov>
2025-10-09 11:03:19 -0700

    11.12.1

--- nmdc_schema/nmdc.py
+++ nmdc_schema/nmdc.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-10-09T10:04:09
+# Generation date: 2025-10-09T11:01:15
 # Schema: NMDC
 #
 # id: https://w3id.org/nmdc/nmdc
