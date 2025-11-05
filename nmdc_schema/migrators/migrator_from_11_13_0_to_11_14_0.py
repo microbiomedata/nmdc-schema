52eaf0ae8c1e444b226b163eebb0263a2bc5ec8d
samobermiller <samobermiller@gmail.com>
2025-11-05 14:20:25 -0800

    turn migrator into partial

--- nmdc_schema/migrators/migrator_from_11_13_0_to_11_14_0.py
+++ nmdc_schema/migrators/migrator_from_11_13_0_to_11_14_0.py
@@ -1,4 +1,7 @@
 from nmdc_schema.migrators.migrator_base import MigratorBase
+from nmdc_schema.migrators.partials.migrator_from_11_11_0_to_11_13_0 import (
+    get_migrator_classes,
+)
 
 
 class Migrator(MigratorBase):
@@ -12,29 +15,21 @@ class Migrator(MigratorBase):
     def upgrade(self, commit_changes: bool = False) -> None:
         r"""
         Migrates the database from conforming to the original schema, to conforming to the new schema.
+
+        This migrator uses partial migrators. It runs them in the order in which they are returned by
+        the `get_migrator_classes` function.
+        
+        Args:
+            commit_changes: If True, commits the changes. If False (default), performs a dry run or rollback.
         """
-        self.adapter.do_for_each_document("processed_sample_set", self.confirm_empty)
 
-    def confirm_empty(self, processedsample: dict) -> None:
-        r"""
-        If a processedsample has a value for 'biomaterial_purity' raise an exception.
-
-        >>> m = Migrator()
-
-        # Test: processed sample has biomaterial_purity value
-        >>> m.confirm_empty({"id": 1, "type": "nmdc:ProcessedSample", "biomaterial_purity": ["Test_Value"]})
-        Traceback (most recent call last):
-            ...
-        ValueError: Biosample 1 'biomaterial_purity' values include: ['Test_Value']
-
-        # Test: valid processedsample
-        >>> m.confirm_empty({"id": 3, "type": "nmdc:ProcessedSample"}) is None
-        True
-        """
-        bp_values = processedsample.get("biomaterial_purity", [])
-        biosample_id = processedsample.get("id")
-
-        if len(bp_values)>0:
-            raise ValueError(
-                f"Biosample {biosample_id} 'biomaterial_purity' values include: {bp_values}"
+        migrator_classes = get_migrator_classes()
+        num_migrators = len(migrator_classes)
+        for idx, migrator_class in enumerate(migrator_classes):
+            self.logger.info(f"Running migrator {idx + 1} of {num_migrators}")
+            self.logger.debug(
+                f"Migrating from {migrator_class.get_origin_version()} "
+                f"to {migrator_class.get_destination_version()}"
             )
+            migrator = migrator_class(adapter=self.adapter, logger=self.logger)
+            migrator.upgrade(commit_changes=commit_changes)
