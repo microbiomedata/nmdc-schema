63d7d988c51b8a7225bd221f07430a5aff1867fd
eecavanna <eecavanna@users.noreply.github.com>
2026-04-22 17:38:30 -0700

    Implement migrator that lists collections we want to validate

---
+++ nmdc_schema/migrators/migrator_from_11_18_0_to_11_19_0.py
@@ -0,0 +1,33 @@
+import re
+from typing import Dict, List
+
+from nmdc_schema.migrators.migrator_base import MigratorBase
+
+
+class Migrator(MigratorBase):
+    r"""Migrates a database between two schemas."""
+
+    _from_version = "11.18.0"
+    _to_version = "11.19.0"
+
+    def upgrade(self, commit_changes: bool = False) -> None:
+        r"""
+        Migrates the database from conforming to the original schema, to conforming to the new schema.
+        """
+
+        # List of collection names that we want the migration orchestration notebook to validate.
+        self._collection_names = [
+            "biosample_set",
+            "workflow_execution_set",
+        ]
+
+        # Note: This migrator does not perform any transformations. It also doesn't perform any
+        #       validation (or "checks"), itself.
+        # 
+        #       However, it does contain the above list of the names of collections whose
+        #       documents we want the migration orchestration notebook to validate during the
+        #       migration process.
+        # 
+        #       This is a stopgap solution for while the following issue is unresolved:
+        #       https://github.com/microbiomedata/nmdc-schema/issues/2674
+        pass
